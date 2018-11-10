a0dfa9b818d439ce71c5b0b8495edae55b6def9e
FABIANO <serge.fabiano@sap.com>
2018-11-10 18:59:00 +0100

    Format code in unit tests

--- test/tests/login/LoginInvalidTest.js
+++ test/tests/login/LoginInvalidTest.js
@@ -1,10 +1,9 @@
 Feature('As an unkown user, I should not be able to login @login');
 
 Scenario('If I try to login, I am notified that my credential is rejected', (I, loginPage) => {
-    let user = I.amANewUser();
-    I.amOnPage('/auth/login');
-    I.wait(1);
-    loginPage.login(user);
-    I.see('Wrong email or password', `//span[@data-notify='message']`);
+  let user = I.amANewUser();
+  I.amOnPage('/auth/login');
+  I.wait(1);
+  loginPage.login(user);
+  I.see('Wrong email or password', `//span[@data-notify='message']`);
 });
-
--- test/tests/login/UserRegistrationTest.js
+++ test/tests/login/UserRegistrationTest.js
@@ -4,22 +4,22 @@ const assert = require('assert');
 let user;
 
 BeforeSuite((I) => {
-    user = I.amANewUser();
+  user = I.amANewUser();
 });
 
 Scenario('I create my account', async (I, loginPage, authLayoutPage, registerPage) => {
-    I.amOnPage(loginPage.url);
-    authLayoutPage.goToRegisterPage();
-    I.waitInUrl(registerPage.url, 5);
-    registerPage.register(user);
-    I.seeInCurrentUrl(loginPage.url);
-    I.see('Your account has been created with success! Check your email', `//span[@data-notify='message']`);
+  I.amOnPage(loginPage.url);
+  authLayoutPage.goToRegisterPage();
+  I.waitInUrl(registerPage.url, 5);
+  registerPage.register(user);
+  I.seeInCurrentUrl(loginPage.url);
+  I.see('Your account has been created with success! Check your email', `//span[@data-notify='message']`);
 });
 
 Scenario('If I try to login, I am notified that my account is not verified', (I, loginPage) => {
-    I.amOnPage(loginPage.url);
-    loginPage.login(user);
-    I.see('Your account is not active! Check your email', `//span[@data-notify='message']`);
+  I.amOnPage(loginPage.url);
+  loginPage.login(user);
+  I.see('Your account is not active! Check your email', `//span[@data-notify='message']`);
 });
 
 Scenario('I validate my mail address', async (I, loginPage, mailboxPage) => {

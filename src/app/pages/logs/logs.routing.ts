433fb3ef8d8c726502075b3f947ed68c46d9d8d9
Sebastien Savalle <sebastien.savalle@sap.com>
2018-11-21 10:50:54 +0100

    clearly split superadmin and admin role.

--- src/app/pages/logs/logs.routing.ts
+++ src/app/pages/logs/logs.routing.ts
@@ -4,5 +4,5 @@ import {LogsComponent} from './logs.component';
 import {RouteGuardService} from '../../services/route-guard.service';
 
 export const LogsRoutes: Routes = [
-  {path: '', component: LogsComponent, canActivate: [RouteGuardService], data: {forAdminOnly: true}}
+  {path: '', component: LogsComponent, canActivate: [RouteGuardService], data: {forAdminOnly: true, forSuperAdminOnly: true}}
 ];

190fca1b8ed915f1c99c4c592fefa0f58517f89a
Sebastien Savalle <sebastien.savalle@sap.com>
2018-11-25 21:07:26 +0100

    Make chips full container width + center text

--- src/app/pages/logs/filters/log-action-filter.ts
+++ src/app/pages/logs/filters/log-action-filter.ts
@@ -1,11 +1,10 @@
 import {TableFilter} from '../../../shared/table/filters/table-filter';
 import {Constants} from '../../../utils/Constants';
-import {CentralServerService} from '../../../services/central-server.service';
 import {TableFilterDef} from '../../../common.types';
+import {logActions} from '../logs.model';
 
 export class LogActionTableFilter extends TableFilter {
-  constructor(
-    private centralServerService: CentralServerService) {
+  constructor() {
     super();
     // Define filter
     const filterDef: TableFilterDef = {
@@ -15,7 +14,7 @@ export class LogActionTableFilter extends TableFilter {
       name: 'logs.action',
       class: 'col-md-6 col-lg-4 col-xl-2',
       currentValue: Constants.FILTER_ALL_KEY,
-      items: this.centralServerService.getLogActions()
+      items: Object.assign([], logActions)
     };
     // Add <All>
     filterDef.items.unshift({key: Constants.FILTER_ALL_KEY, value: 'general.all'});
--- src/app/pages/logs/filters/log-level-filter.ts
+++ src/app/pages/logs/filters/log-level-filter.ts
@@ -1,11 +1,10 @@
 import {Constants} from '../../../utils/Constants';
-import {CentralServerService} from '../../../services/central-server.service';
 import {TableFilter} from '../../../shared/table/filters/table-filter';
 import {TableFilterDef} from '../../../common.types';
+import {logLevels} from '../logs.model';
 
 export class LogLevelTableFilter extends TableFilter {
-  constructor(
-    private centralServerService: CentralServerService) {
+  constructor() {
     super();
     // Define filter
     const filterDef: TableFilterDef = {
@@ -15,7 +14,7 @@ export class LogLevelTableFilter extends TableFilter {
       name: 'logs.level',
       class: 'col-sm-4 col-md-3 col-lg-2 col-xl-1',
       currentValue: Constants.FILTER_ALL_KEY,
-      items: this.centralServerService.getLogStatus()
+      items: Object.assign([], logLevels)
     };
     filterDef.items.unshift({key: Constants.FILTER_ALL_KEY, value: 'general.all'});
     // Set

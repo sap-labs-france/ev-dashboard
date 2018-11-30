a5b63855573469dd1eb6760352f1528c6341e38f
Sebastien Savalle <sebastien.savalle@sap.com>
2018-11-30 16:36:11 +0100

    fix pipe

--- src/app/pages/users/formatters/user-role.pipe.ts
+++ src/app/pages/users/formatters/user-role.pipe.ts
@@ -5,8 +5,7 @@ import {memoize} from 'decko';
 @Pipe({name: 'userRole'})
 export class UserRolePipe implements PipeTransform {
   @memoize
-  transform(role: string, ...args: any[]): any {
-    const currentRole = args && args[0] ? args[0] : '';
+  transform(role: string, currentRole = ''): any {
     for (const userRole of UserRoles.getAvailableRoles(currentRole)) {
       if (userRole.key === role) {
         return userRole.value;

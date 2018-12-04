a2e428e6170ab553bda3184910ad33c0ef4e8449
PERNICE <florent.pernice@sap.com>
2018-12-04 14:13:47 +0100

    remove functions call in template

--- src/app/pages/charging-stations/cell-content-components/connector-cell.component.ts
+++ src/app/pages/charging-stations/cell-content-components/connector-cell.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, AfterViewInit, OnInit, AfterContentInit, Injectable } from '@angular/core';
+import { Component, Input, Injectable } from '@angular/core';
 import { TableColumnDef, Connector } from '../../../common.types';
 import { CellContentTemplateComponent } from '../../../shared/table/cell-content-template/cell-content-template.component';
 @Component({
   selector: 'connector-id-cell',
   template: `
-      <div class="charger-connector charger-connector-background {{getClassForStatus(row.status)}}">
-        <span [class]="getTextClassForStatus(row.status)">
+      <div data-toggle="tooltip" [attr.title]="row.status" class="charger-connector charger-connector-background" [appChargerStatus]="row.status">
+        <span [appChargerStatusText]="row.status">
           {{row.connectorId | appConnectorId}}
         </span>
       </div>
@@ -13,69 +13,8 @@ import { CellContentTemplateComponent } from '../../../shared/table/cell-content
 })
 
 @Injectable()
-export class ConnectorCellComponent implements CellContentTemplateComponent, OnInit {
+export class ConnectorCellComponent implements CellContentTemplateComponent {
 
   @Input() row: any;
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-//    this.tooltip = this.translateService.instant('connector_status_tooltip');
-  }
   
-  getClassForStatus(status: string) {
-    switch (status) {
-      case "Available": {
-        return "charger-connector-available";
-      }
-      case "Preparing": {
-        return "charger-connector-preparing";
-      }
-      case "SuspendedEVSE": {
-        return "charger-connector-suspended-evse";
-      }
-      case "SuspendedEV": {
-        return "charger-connector-suspended-ev";
-      }
-      case "Finishing": {
-        return "charger-connector-finishing";
-      }
-      case "Reserved": {
-        return "charger-connector-reserved";
-      }
-      case "Charging":
-      case "Occupied": {
-        return "charger-connector-charging";
-      }
-      case "Unavailable": {
-        return "charger-connector-unavailable";
-      }
-      case "Faulted": {
-        return "charger-connector-faulted";
-      }
-      default: {
-        return "'charger-connector-active-text'";
-      }
-    }
-  }
-
-  getTextClassForStatus(status: String) {
-    switch (status) {
-      case "Reserved":
-      case "Available": {
-        return "charger-connector-text";
-      }
-      case "Finishing":
-      case "Preparing":
-      case "SuspendedEVSE":
-      case "SuspendedEV":
-      case "Charging":
-      case "Occupied":
-      case "Unavailable":
-      case "Faulted":
-      default: {
-        return "charger-connector-active-text";
-      }
-    }
-  }
 }
--- src/app/shared/directives/common-directives.module.ts
+++ src/app/shared/directives/common-directives.module.ts
@@ -1,16 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AppAutofocusDirective} from './app-auto-focus.directive';
+import {ChargerStatusDirective} from "./charger-status.directive";
+import {ChargerStatusTextDirective} from "./charger-status-text.directive";
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    AppAutofocusDirective
+    AppAutofocusDirective,
+    ChargerStatusDirective,
+    ChargerStatusTextDirective
   ],
   exports: [
-    AppAutofocusDirective
+    AppAutofocusDirective,
+    ChargerStatusDirective,
+    ChargerStatusTextDirective
   ]
 })
 export class CommonDirectivesModule {

a47d4530345b3d1210edafa800dd0ec2aebe52a7
Sebastien Savalle <sebastien.savalle@sap.com>
2019-01-28 19:40:44 +0100

    adapt charging stations to use flex

--- src/app/pages/charging-stations/charging-stations.component.ts
+++ src/app/pages/charging-stations/charging-stations.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from 'app/services/authorization-service';
+import {AbstractTabComponent} from '../../shared/component/tab/AbstractTab.component';
+import {ActivatedRoute} from '@angular/router';
+import {WindowService} from '../../services/window.service';
 
 @Component({
   templateUrl: 'charging-stations.component.html'
 })
-export class ChargingStationsComponent implements OnInit {
+export class ChargingStationsComponent extends AbstractTabComponent implements OnInit {
   isAdmin: boolean;
   constructor (
-    private authorizationService: AuthorizationService
+    private authorizationService: AuthorizationService,
+    activatedRoute: ActivatedRoute,
+    windowService: WindowService
   ) {
+    super(activatedRoute, windowService, ['all', 'inerror']);
     this.isAdmin = this.authorizationService.isAdmin() || this.authorizationService.isSuperAdmin();
   }
 

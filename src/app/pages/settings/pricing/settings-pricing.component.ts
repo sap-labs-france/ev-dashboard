2ab370a0962a826bf86cfdd99e5439d7fd554ac6
PALAZZOLO Patrice <patrice.palazzolo@free.fr>
2019-03-18 17:00:47 +0100

    Component - update Descr for SAC and ROmaing - fix error in pricing

--- src/app/pages/settings/pricing/settings-pricing.component.ts
+++ src/app/pages/settings/pricing/settings-pricing.component.ts
@@ -119,8 +119,8 @@ export class SettingsPricingComponent implements OnInit {
           this.currency.setValue(config.simple.currency ? config.simple.currency : '');
         }
       }
-      this.formGroup.markAsPristine();
 
+      if (this.formGroup) { this.formGroup.markAsPristine(); }
     }, (error) => {
       this.spinnerService.hide();
       switch (error.status) {

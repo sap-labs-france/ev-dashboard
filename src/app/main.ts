7815d7cd9b0cba17f8b0f3aa2a88f933c90ae2de
Jérôme Benoit <jerome.benoit@piment-noir.org>
2019-03-24 17:27:19 +0100

    Enable prod mode only in the production environment.
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,9 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 import {enableProdMode} from '@angular/core';
+import {environment} from 'environments/environment';
 
-
-enableProdMode();
+if (environment.production) {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);

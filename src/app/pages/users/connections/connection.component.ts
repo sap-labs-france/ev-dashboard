4af36bd59d1c833dfa6d953722fccbf39d1cccf7
Sebastien Savalle <sebastien.savalle@sap.com>
2019-04-02 13:41:33 +0200

    clean code

--- src/app/pages/users/connections/connection.component.ts
+++ src/app/pages/users/connections/connection.component.ts
@@ -1,26 +1,19 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {MatDialog} from '@angular/material';
-import {LocaleService} from '../../../services/locale.service';
 import {CentralServerService} from '../../../services/central-server.service';
-import {SpinnerService} from '../../../services/spinner.service';
-import {AuthorizationService} from '../../../services/authorization-service';
 import {MessageService} from '../../../services/message.service';
-import {DialogService} from '../../../services/dialog.service';
 import {Constants} from '../../../utils/Constants';
 import {Utils} from '../../../utils/Utils';
 import {DOCUMENT} from '@angular/common';
 import {ActionResponse} from '../../../common.types';
 import {WindowService} from '../../../services/window.service';
 import {AbstractTabComponent} from '../../../shared/component/tab/AbstractTab.component';
-import {ConfigService} from '../../../services/config.service';
-import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-connection-cmp',
   templateUrl: 'connection.component.html'
 })
-export class ConnectionComponent extends AbstractTabComponent implements OnInit {
+export class ConnectionComponent extends AbstractTabComponent {
   public isAdmin;
 
   constructor(
@@ -41,13 +34,6 @@ export class ConnectionComponent extends AbstractTabComponent implements OnInit 
     }
   }
 
-  ngOnInit() {
-  }
-
-
-  public refresh() {
-  }
-
   private _createConcurConnection(state) {
     if (this.activatedRoute.snapshot.queryParams['code']) {
       const payload = {
@@ -57,15 +43,12 @@ export class ConnectionComponent extends AbstractTabComponent implements OnInit 
         data:
           {
             code: this.activatedRoute.snapshot.queryParams['code'],
-            // redirectUri: 'https://slfcah.evse.cfapps.eu10.hana.ondemand.com/users/connections'
-            redirectUri: this.windowService.getOrigin()
+            redirectUri: this.windowService.getOrigin() + this.windowService.getPath()
           }
       };
       this.centralServerService.createIntegrationConnection(payload).subscribe((response: ActionResponse) => {
           if (response.status === Constants.REST_RESPONSE_SUCCESS) {
-            // Ok
             this.messageService.showSuccessMessage('settings.chargeathome.concur.link_success');
-            // this.loadApplicationSettings();
           } else {
             Utils.handleError(JSON.stringify(response),
               this.messageService, 'settings.chargeathome.concur.link_error');

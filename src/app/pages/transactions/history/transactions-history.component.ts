6a301a8745534e6fc7a907238fbc364257d6c43e
DEMESSANT <jean.pierre.demessant@sap.com>
2019-04-04 11:10:04 +0200

    #321-Email-Transaction-Show-Current-Transaction
    Version initiale

--- src/app/pages/transactions/history/transactions-history.component.ts
+++ src/app/pages/transactions/history/transactions-history.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthorizationService} from '../../../services/authorization-service';
 import {TransactionsHistoryDataSource} from './transactions-history-data-source-table';
+import { CentralServerService } from 'app/services/central-server.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-transactions-history',
@@ -9,14 +11,28 @@ import {TransactionsHistoryDataSource} from './transactions-history-data-source-
     TransactionsHistoryDataSource
   ]
 })
-export class TransactionsHistoryComponent {
+export class TransactionsHistoryComponent implements OnInit {
   public isAdmin;
+  private transactionId;
 
   constructor(
     public transactionsHistoryDataSource: TransactionsHistoryDataSource,
-    private authorizationService: AuthorizationService
+    private authorizationService: AuthorizationService,
+    private centralServerService: CentralServerService,
+    private activatedRoute: ActivatedRoute
   ) {
     this.isAdmin = this.authorizationService.isAdmin();
     this.transactionsHistoryDataSource.forAdmin(this.isAdmin);
+  }
+
+  ngOnInit(): void{
+    this.transactionId = this.activatedRoute.snapshot.queryParams['TransactionID'];
+    if(this.transactionId){
+      this.centralServerService.getTransaction(this.transactionId).subscribe(transaction => {
+        if(transaction) {
+          this.transactionsHistoryDataSource.openSession(transaction);
+        }
+      })
+    }
   }
 }

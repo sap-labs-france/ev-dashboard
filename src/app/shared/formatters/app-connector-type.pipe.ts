a3e7f9cf1790ec2c41f443ab4e339f8cbd0368eb
PERNICE <florent.pernice@sap.com>
2019-04-25 09:53:42 +0200

    fix no connector svg usage

--- src/app/shared/formatters/app-connector-type.pipe.ts
+++ src/app/shared/formatters/app-connector-type.pipe.ts
@@ -5,7 +5,7 @@ export const CONNECTOR_TYPE_MAP =
     {key: 'T2', description: 'chargers.connector_type_type2', svgIconName: 'T2', svgIconFile: 'assets/img/connectors/type2.svg'},
     {key: 'CCS', description: 'chargers.connector_type_combo', svgIconName: 'CCS', svgIconFile: 'assets/img/connectors/combo_ccs.svg'},
     {key: 'C', description: 'chargers.connector_type_chademo', svgIconName: 'C', svgIconFile: 'assets/img/connectors/chademo.svg'},
-    {key: 'U', description: 'chargers.connector_type_unknown', image: 'assets/img/connectors/no-connector.gif'}
+    {key: 'U', description: 'chargers.connector_type_unknown', svgIconName: 'U', svgIconFile: 'assets/img/connectors/no-connector.svg'}
   ]
 
 /**
@@ -17,12 +17,12 @@ export const CONNECTOR_TYPE_MAP =
 @Pipe({name: 'appConnectorType'})
 export class AppConnectorTypePipe implements PipeTransform {
 
-  transform(type: string, asIconUrl: boolean = true): any {
+  transform(type: string, asSvgIcon: boolean = true): any {
     // Return the found key
     const foundConnectorType = CONNECTOR_TYPE_MAP.find(
       (connectorType) => connectorType.key === type);
-    if (asIconUrl) {
-      return (foundConnectorType ? foundConnectorType.svgIconName : 'not_interested');
+    if (asSvgIcon) {
+        return (foundConnectorType ? foundConnectorType.svgIconName : '');
     } else {
       return (foundConnectorType ? foundConnectorType.description : 'chargers.connector_type_unknown');
     }

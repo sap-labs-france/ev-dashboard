ca5d4847928f9fa9e26bb98e1c17da7403dfef44
DEMESSANT <jean.pierre.demessant@sap.com>
2019-05-23 11:25:57 +0200

    #537-All-List-Show-ID-Admin-
    #537

--- src/app/pages/users/users-data-source-table.ts
+++ src/app/pages/users/users-data-source-table.ts
@@ -31,6 +31,7 @@ import { SpinnerService } from 'app/services/spinner.service';
 
 @Injectable()
 export class UsersDataSource extends TableDataSource<User> {
+
   constructor(
       public spinnerService: SpinnerService,
       private messageService: MessageService,
@@ -81,71 +82,78 @@ export class UsersDataSource extends TableDataSource<User> {
 
   public buildTableColumnDefs(): TableColumnDef[] {
     const loggedUserRole = this.centralServerService.getLoggedUser().role;
-    return [
-      {
-        id: 'status',
-        name: 'users.status',
-        isAngularComponent: true,
-        angularComponent: UserStatusComponent,
-        headerClass: 'col-10p',
-        class: 'col-10p',
-        sortable: true
-      },
-      {
-        id: 'role',
-        name: 'users.role',
-        formatter: (role) => this.translateService.instant(this.userRolePipe.transform(role, loggedUserRole)),
-        headerClass: 'col-10p',
-        class: 'text-left col-10p',
-        sortable: true
-      },
-      {
-        id: 'name',
-        name: 'users.name',
-        headerClass: 'col-15p',
-        class: 'text-left col-15p',
-        sorted: true,
-        direction: 'asc',
-        sortable: true
-      },
-      {
-        id: 'firstName',
-        name: 'users.first_name',
-        headerClass: 'col-15p',
-        class: 'text-left col-15p',
-        sortable: true
-      },
-      {
-        id: 'email',
-        name: 'users.email',
-        headerClass: 'col-20p',
-        class: 'col-20p',
-        sortable: true
-      },
-      {
-        id: 'tagIDs',
-        name: 'users.tag_ids',
-        formatter: this.arrayToStringPipe.transform,
-        headerClass: 'col-15p',
-        class: 'col-15p',
-        sortable: true
-      },
-      {
-        id: 'plateID',
-        name: 'users.plate_id',
-        headerClass: 'col-10p',
-        class: 'col-10p',
-        sortable: true
-      },
-      {
-        id: 'createdOn',
-        name: 'users.created_on',
-        formatter: (createdOn) => this.datePipe.transform(createdOn),
-        headerClass: 'col-15p',
-        class: 'col-15p',
-        sortable: true
-      }
-    ];
+    const columns = [];
+    columns.push(
+    {
+      id: 'id',
+      name: 'transactions.id',
+      headerClass: 'd-none d-xl-table-cell',
+      class: 'd-none d-xl-table-cell',
+    },
+    {
+      id: 'status',
+      name: 'users.status',
+      isAngularComponent: true,
+      angularComponent: UserStatusComponent,
+      headerClass: 'col-10p',
+      class: 'col-10p',
+      sortable: true
+    },
+    {
+      id: 'role',
+      name: 'users.role',
+      formatter: (role) => this.translateService.instant(this.userRolePipe.transform(role, loggedUserRole)),
+      headerClass: 'col-10p',
+      class: 'text-left col-10p',
+      sortable: true
+    },
+    {
+      id: 'name',
+      name: 'users.name',
+      headerClass: 'col-15p',
+      class: 'text-left col-15p',
+      sorted: true,
+      direction: 'asc',
+      sortable: true
+    },
+    {
+      id: 'firstName',
+      name: 'users.first_name',
+      headerClass: 'col-15p',
+      class: 'text-left col-15p',
+      sortable: true
+    },
+    {
+      id: 'email',
+      name: 'users.email',
+      headerClass: 'col-20p',
+      class: 'col-20p',
+      sortable: true
+    },
+    {
+      id: 'tagIDs',
+      name: 'users.tag_ids',
+      formatter: this.arrayToStringPipe.transform,
+      headerClass: 'col-15p',
+      class: 'col-15p',
+      sortable: true
+    },
+    {
+      id: 'plateID',
+      name: 'users.plate_id',
+      headerClass: 'col-10p',
+      class: 'col-10p',
+      sortable: true
+    },
+    {
+      id: 'createdOn',
+      name: 'users.created_on',
+      formatter: (createdOn) => this.datePipe.transform(createdOn),
+      headerClass: 'col-15p',
+      class: 'col-15p',
+      sortable: true
+    });
+    return columns as TableColumnDef[];
   }
 
   public buildTableActionsDef(): TableActionDef[] {

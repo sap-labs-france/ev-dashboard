9b4acd660b580a3f8cc0b881552bc3bbfc9906e1
FABIANO <serge.fabiano@sap.com>
2019-06-08 21:37:09 +0200

    Added log actions

--- src/app/pages/logs/logs.model.ts
+++ src/app/pages/logs/logs.model.ts
@@ -38,6 +38,7 @@ export const logActions: KeyValue[] = [
   {key: 'NotifyUnknownUserBadged', value: 'NotifyUnknownUserBadged'},
   {key: 'NotifyUserAccountStatusChanged', value: 'NotifyUserAccountStatusChanged'},
   {key: 'OCPIPatchLocations', value: 'OCPIPatchLocations'},
+  {key: 'OCPISendEVSEStatuses', value: 'OCPISendEVSEStatuses'},
   {key: 'PricingUpdate', value: 'PricingUpdate'},
   {key: 'RegisterUser', value: 'RegisterUser'},
   {key: 'VerifyEmail', value: 'VerifyEmail'},
@@ -47,7 +48,6 @@ export const logActions: KeyValue[] = [
   {key: 'RequestConfiguration', value: 'RequestConfiguration'},
   {key: 'Reset', value: 'Reset'},
   {key: 'SendEmail', value: 'SendEmail'},
-  {key: 'SendEVSEStatuses', value: 'SendEVSEStatuses'},
   {key: 'SetChargingProfile', value: 'SetChargingProfile'},
   {key: 'SessionHashHandling', value: 'SessionHashHandling'},
   {key: 'SiteAreaCreate', value: 'SiteAreaCreate'},
@@ -85,7 +85,8 @@ export const logActions: KeyValue[] = [
   {key: 'Startup', value: 'Startup'},
   {key: 'StrongSoapDebug', value: 'StrongSoapDebug'},
   {key: 'SecurePing', value: 'SecurePing'},
-  {key: 'UserUpdate', value: 'UserUpdate'}
+  {key: 'UserUpdate', value: 'UserUpdate'},
+  {key: 'SettingUpdate', value: 'SettingUpdate'}
 ].sort((action1, action2) => {
   if (action1.value.toLocaleLowerCase() < action2.value.toLocaleLowerCase()) {
     return -1;

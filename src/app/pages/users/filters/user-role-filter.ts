e8959292103a900b332ba4c64653f14b9646c081
DEMESSANT <jean.pierre.demessant@sap.com>
2019-07-26 10:50:33 +0200

    #444-Filter-Implement-multi-selection
    Users

--- src/app/pages/users/filters/user-role-filter.ts
+++ src/app/pages/users/filters/user-role-filter.ts
@@ -15,11 +15,10 @@ export class UserRoleFilter extends TableFilter {
       type: Constants.FILTER_TYPE_DROPDOWN,
       name: 'users.role',
       class: 'col-md-6 col-lg-4 col-xl-2',
-      currentValue: Constants.FILTER_ALL_KEY,
-      items: UserRoles.getAvailableRoles(this.centralServerService.getLoggedUser().role)
+      currentValue: [],
+      items: UserRoles.getAvailableRoles(this.centralServerService.getLoggedUser().role),
+      multiple: true
     };
-    // Add <All>
-    filterDef.items.unshift({key: Constants.FILTER_ALL_KEY, value: 'general.all'});
     this.setFilterDef(filterDef);
   }
 }

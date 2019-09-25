f369d335d9cdb7dacc3c95796556f4e3c1947c83
System Administrator <sergefabiano@free.fr>
2019-09-25 23:54:42 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -25,11 +25,12 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.1.9',
-        date: new Date('2019-09-24'),
+        date: new Date('2019-09-25'),
         componentChanges: [
           {
             name: 'Dashboard',
             changes: [
+              `Add enpoint for Mobile App to enable Auto Login feature`,
               `Fixed bottom list's stats in Refunded Sessions to take into account cancelled expenses`,
               `Fixed Charging Station's connection token cannot be revoked when it has already expired`,
               `Fixed 'More' button in list disappears when Log's Message column is too wide`,

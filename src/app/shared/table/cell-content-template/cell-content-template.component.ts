3d7d46349528517cb941403ea86aa1229dc5bcc3
sebastien.savalle@gmail.com <sebastien.savalle@gmail.com>
2019-11-18 12:16:16 +0100

    Fix build

--- src/app/shared/table/cell-content-template/cell-content-template.component.ts
+++ src/app/shared/table/cell-content-template/cell-content-template.component.ts
@@ -1,7 +1,10 @@
-import { Input } from '@angular/core';
+import { Input, OnChanges, SimpleChanges } from '@angular/core';
 import { TableColumnDef } from '../../../common.types';
 
-export abstract class CellContentTemplateComponent {
+export abstract class CellContentTemplateComponent implements OnChanges {
   @Input() row: any;
   @Input() columnDef: TableColumnDef;
+
+  // tslint:disable-next-line:no-empty
+  ngOnChanges(changes: SimpleChanges): void {}
 }

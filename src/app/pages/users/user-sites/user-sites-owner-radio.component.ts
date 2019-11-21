23f3726221db2b80b6a899e51e7b9314bd45d3d8
OliveGerste <33114317+OliveGerste@users.noreply.github.com>
2019-11-21 16:26:55 +0100

    Site Owner added to user management

---
+++ src/app/pages/users/user-sites/user-sites-owner-radio.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material';
+import { MatRadioChange } from '@angular/material/radio';
+import { Router } from '@angular/router';
+import { User, UserSite, UserToken, SiteUser } from '../../../common.types';
+import { CentralServerService } from '../../../services/central-server.service';
+import { MessageService } from '../../../services/message.service';
+import { CellContentTemplateComponent } from '../../../shared/table/cell-content-template/cell-content-template.component';
+import { Constants } from '../../../utils/Constants';
+import { Utils } from '../../../utils/Utils';
+
+@Component({
+  template: `
+      <div class="d-flex justify-content-center">
+          <mat-checkbox class="mx-auto"
+                            [checked]="(row.siteOwner ? row.siteOwner : false)"
+                            [disabled]="(row.siteOwner ? row.siteOwner : false)"
+                            (change)="changeSiteOwner($event)"></mat-checkbox>
+      </div>`,
+})
+export class UserSitesOwnerRadioComponent extends CellContentTemplateComponent {
+  @Input() row: SiteUser;
+  public loggedUser: UserToken;
+
+  constructor(
+    private messageService: MessageService,
+    private centralServerService: CentralServerService,
+    private router: Router) {
+    super();
+    this.loggedUser = centralServerService.getLoggedUser();
+  }
+
+  public changeSiteOwner(matRadioChange: MatRadioChange) {
+    if (matRadioChange) {
+      matRadioChange.source.disabled = true;
+      this.setUserSiteOwner(this.row);
+    }
+  }
+
+  private setUserSiteOwner(siteUser: SiteUser) {
+    // Update
+    this.centralServerService.updateSiteOwner(siteUser.site.id, siteUser.userID).subscribe((response) => {
+        if (response.status === Constants.REST_RESPONSE_SUCCESS) {
+          this.messageService.showSuccessMessage('users.update_set_site_owner_success', {siteName: siteUser.site.name});
+        } else {
+          Utils.handleError(JSON.stringify(response),
+            this.messageService, 'users.update_site_users_owner_error', {
+              siteName: siteUser.site.name,
+            });
+        }
+      }
+      ,
+      (error) => {
+        Utils.handleHttpError(error, this.router, this.messageService, this.centralServerService,
+          'users.update_site_users_owner_error', {siteName: siteUser.site.name});
+      },
+    );
+  }
+}

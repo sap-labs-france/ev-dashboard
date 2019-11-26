d20f6443565a3b895594d85e1c2159e9b8b3d74e
OliveGerste <33114317+OliveGerste@users.noreply.github.com>
2019-11-26 08:58:32 +0100

    #902-Site-Admin-Implement-Site-Owner-to-get-sessions-refunded-with-Concur
    fixed syntax

--- src/app/pages/users/user-sites/user-sites-owner-radio.component.ts
+++ src/app/pages/users/user-sites/user-sites-owner-radio.component.ts
@@ -18,7 +18,7 @@ import { Utils } from '../../../utils/Utils';
       </div>`,
 })
 export class UserSitesOwnerRadioComponent extends CellContentTemplateComponent {
-  @Input() row: SiteUser;
+  @Input() row!: SiteUser;
   public loggedUser: UserToken;
 
   constructor(

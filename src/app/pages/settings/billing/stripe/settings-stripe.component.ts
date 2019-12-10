544f258c407cc100fa3df04968f3305f536e1c7b
LittlePinkCookie <alouache.loan@gmail.com>
2019-12-10 14:46:06 +0100

    Better way to manage controls

--- src/app/pages/settings/billing/stripe/settings-stripe.component.ts
+++ src/app/pages/settings/billing/stripe/settings-stripe.component.ts
@@ -30,55 +30,57 @@ export class SettingsStripeComponent implements OnInit, OnChanges {
   public taxCode: AbstractControl;
 
   ngOnInit() {
-    this.stripe =
-      new FormGroup({
-        url: new FormControl('',
-          Validators.compose([
-            Validators.required,
-            Validators.maxLength(200),
-            Validators.pattern(Constants.URL_PATTERN),
-          ]),
-        ),
-        secretKey: new FormControl('',
-          Validators.compose([
-            Validators.required,
-            Validators.maxLength(100),
-            //  StripeToolBox.validateSecretKey
-          ]),
-        ),
-        publicKey: new FormControl('',
-          Validators.compose([
-            Validators.required,
-            Validators.maxLength(100),
-            StripeToolBox.validatePublicKey,
-          ]),
-        ),
-        immediateBillingAllowed: new FormControl(''),
-        periodicBillingAllowed: new FormControl(''),
-        lastSynchronizedOn: new FormControl(''),
-        taxCountry: new FormControl('',
-          Validators.compose([
-            Validators.required,
-          ]),
-        ),
-        taxCode: new FormControl('',
-          Validators.compose([
-            Validators.required,
-            Validators.maxLength(20),
-            StripeToolBox.validateTaxCode,
-          ]),
-        ),
-      });
+    this.stripe = new FormGroup({
+      url: new FormControl('',
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(200),
+          Validators.pattern(Constants.URL_PATTERN),
+        ]),
+      ),
+      secretKey: new FormControl('',
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(100),
+          //  StripeToolBox.validateSecretKey
+        ]),
+      ),
+      publicKey: new FormControl('',
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(100),
+          StripeToolBox.validatePublicKey,
+        ]),
+      ),
+      immediateBillingAllowed: new FormControl(''),
+      periodicBillingAllowed: new FormControl(''),
+      lastSynchronizedOn: new FormControl(''),
+      taxCountry: new FormControl('',
+        Validators.compose([
+          Validators.required,
+        ]),
+      ),
+      taxCode: new FormControl('',
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(20),
+          StripeToolBox.validateTaxCode,
+        ]),
+      ),
+    });
+
+    this.formGroup.addControl('stripe', this.stripe);
+
     // Keep
-    this.stripeUrl = this.stripe.controls['url'];
-    this.stripeSecretKey = this.stripe.controls['secretKey'];
-    this.stripePublicKey = this.stripe.controls['publicKey'];
-    this.stripeImmediateBillingAllowed = this.stripe.controls['immediateBillingAllowed'];
-    this.stripePeriodicBillingAllowed = this.stripe.controls['periodicBillingAllowed'];
-    this.stripeLastSynchronizedOn = this.stripe.controls['lastSynchronizedOn'];
-    this.taxCountry = this.stripe.controls['taxCountry'];
+    this.stripeUrl = (this.formGroup.get('stripe') as FormGroup).controls['url'];
+    this.stripeSecretKey = (this.formGroup.get('stripe') as FormGroup).controls['secretKey'];
+    this.stripePublicKey = (this.formGroup.get('stripe') as FormGroup).controls['publicKey'];
+    this.stripeImmediateBillingAllowed = (this.formGroup.get('stripe') as FormGroup).controls['immediateBillingAllowed'];
+    this.stripePeriodicBillingAllowed = (this.formGroup.get('stripe') as FormGroup).controls['periodicBillingAllowed'];
+    this.stripeLastSynchronizedOn = (this.formGroup.get('stripe') as FormGroup).controls['lastSynchronizedOn'];
+    this.taxCountry = (this.formGroup.get('stripe') as FormGroup).controls['taxCountry'];
     this.taxCountry.setValue('none');
-    this.taxCode = this.stripe.controls['taxCode'];
+    this.taxCode = (this.formGroup.get('stripe') as FormGroup).controls['taxCode'];
     this.taxCode.disable();
 
     // Set data
@@ -110,16 +112,24 @@ export class SettingsStripeComponent implements OnInit, OnChanges {
 
   updateFormData(firstTime = false) {
     // Set data
-    if (this.stripe) {
+    if (this.stripeUrl) {
       this.stripeUrl.setValue(this.billingSettings.stripe.url ? this.billingSettings.stripe.url : '');
-      //      if (firstTime) {
+    }
+    if (this.stripeSecretKey) {
       this.stripeSecretKey.setValue(this.billingSettings.stripe.secretKey ? this.billingSettings.stripe.secretKey : '');
-      //      }
+    }
+    if (this.stripePublicKey) {
       this.stripePublicKey.setValue(this.billingSettings.stripe.publicKey ? this.billingSettings.stripe.publicKey : '');
+    }
+    if (this.stripeImmediateBillingAllowed) {
       this.stripeImmediateBillingAllowed.setValue(this.billingSettings.stripe.immediateBillingAllowed
         ? this.billingSettings.stripe.immediateBillingAllowed : '');
+    }
+    if (this.stripePeriodicBillingAllowed) {
       this.stripePeriodicBillingAllowed.setValue(this.billingSettings.stripe.periodicBillingAllowed
         ? this.billingSettings.stripe.periodicBillingAllowed : '');
+    }
+    if (this.stripeLastSynchronizedOn) {
       this.stripeLastSynchronizedOn.setValue(this.billingSettings.stripe.lastSynchronizedOn
         ? this.billingSettings.stripe.lastSynchronizedOn : '');
     }

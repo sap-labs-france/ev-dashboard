6efbad420f4ad2ce6bed85618836dda021b131a0
FABIANO <sergefabiano@free.fr>
2020-01-15 10:32:28 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -25,7 +25,7 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.1.19',
-        date: new Date('2020-01-14'),
+        date: new Date('2020-01-15'),
         componentChanges: [
           {
             name: 'Dashboard',
@@ -43,6 +43,7 @@ export class ReleaseNotesComponent {
               `Notification: Admins are notified when billing user's synchronization has failed`,
               `All Lists: Optimized performances by optimizing third party libs import usages`,
               `Connector's transaction is not clean until it becomes available`,
+              `Move the action row at the beginning of the list template`,
               `TypeScript: Alignment between front-end and backend types`,
               `TypeScript: migration of Settings`,
               `TypeScript migration of Logging`,

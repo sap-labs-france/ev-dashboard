d6c1ba2c31e7bcdf62bccfb3799bd9540a50bf59
FABIANO <sergefabiano@free.fr>
2020-01-23 00:22:01 +0100

    Sync Concur new types with backend

--- src/app/pages/transactions/history/transactions-history-table-data-source.ts
+++ src/app/pages/transactions/history/transactions-history-table-data-source.ts
@@ -8,6 +8,7 @@ import { SiteTableFilter } from 'app/shared/table/filters/site-table-filter';
 import { Connector } from 'app/types/ChargingStation';
 import { ActionResponse, DataResult, TransactionDataResult } from 'app/types/DataResult';
 import { ButtonAction, SubjectInfo } from 'app/types/GlobalType';
+import { RefundStatus } from 'app/types/Refund';
 import { TableActionDef, TableColumnDef, TableDef, TableFilterDef } from 'app/types/Table';
 import { Transaction, TransactionButtonAction } from 'app/types/Transaction';
 import { User } from 'app/types/User';
@@ -267,8 +268,8 @@ export class TransactionsHistoryTableDataSource extends TableDataSource<Transact
   rowActionTriggered(actionDef: TableActionDef, transaction: Transaction) {
     switch (actionDef.id) {
       case ButtonAction.DELETE:
-        if (transaction.refundData && (transaction.refundData.status === Constants.REFUND_STATUS_SUBMITTED ||
-          transaction.refundData.status === Constants.REFUND_STATUS_APPROVED)) {
+        if (transaction.refundData && (transaction.refundData.status === RefundStatus.SUBMITTED ||
+          transaction.refundData.status === RefundStatus.APPROVED)) {
           this.dialogService.createAndShowOkDialog(
             this.translateService.instant('transactions.dialog.delete.title'),
             this.translateService.instant('transactions.dialog.delete.rejected_refunded_msg'));

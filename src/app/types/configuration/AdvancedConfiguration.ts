e5232e19370d585328e3c827be83ccd032d4e938
Liam <liam.fabiano@sap.com>
2020-02-04 18:42:39 +0100

    Interfaces declared on each config file

---
+++ src/app/types/configuration/AdvancedConfiguration.ts
@@ -0,0 +1,4 @@
+export default interface AdvancedConfiguration {
+  debounceTimeNotifMillis: number;
+  debounceTimeSearchMillis: number;
+}
---
+++ src/app/types/configuration/UserConfiguration.ts
@@ -0,0 +1,6 @@
+export default interface UserConfiguration {
+  maxPictureKb: number;
+  captchaSiteKeyUI: string;
+  captchaSiteKeyUnitTest: string;
+  captchaSiteKey: string;
+}

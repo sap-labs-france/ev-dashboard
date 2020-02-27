12bbed8b0ad4d4044623335a1b31cc3a342093bc
FABIANO <sergefabiano@free.fr>
2020-02-27 22:59:59 +0100

    Changed version

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -24,8 +24,8 @@ export class ReleaseNotesComponent {
   buidlReleaseNotes() {
     this.releaseNotes = [
       {
-        version: '2.2.2',
-        date: new Date('2020-02-26'),
+        version: '2.2.3',
+        date: new Date('2020-02-27'),
         componentChanges: [
           {
             name: 'Dashboard',

ace34d663e8cec59c9156a94b836d65fe06c42ce
FABIANO <sergefabiano@free.fr>
2020-03-08 11:52:43 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -25,6 +25,18 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.2.8',
+        date: new Date('2020-03-08'),
+        componentChanges: [
+          {
+            name: 'Dashboard',
+            changes: [
+              `eMSP/CPO implementation`,
+            ],
+          },
+        ],
+      },
+      {
+        version: '2.2.7',
         date: new Date('2020-03-07'),
         componentChanges: [
           {

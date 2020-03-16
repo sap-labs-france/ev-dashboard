f4c1de5197852b67a284216da880ea20b5a96a7f
FABIANO <sergefabiano@free.fr>
2020-03-16 23:13:22 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -33,6 +33,7 @@ export class ReleaseNotesComponent {
               `Added filters on Connector Types (Type 2, Chademo...)`,
               `Fixed number of values in x-axis in chart consumption`,
               `Changed I-Number to Corporate Number in User's prodile`,
+              `Roaming: Implement PUT and PATCH token from IOP`,
             ],
           },
         ],

2d097034669006979428f325bc6d00439cfac74e
FABIANO <sergefabiano@free.fr>
2020-04-07 10:42:23 +0200

    Added synchronize button in Refund list

---
+++ src/app/shared/table/actions/table-sync-refund-action.ts
@@ -0,0 +1,52 @@
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { CentralServerService } from 'app/services/central-server.service';
+import { DialogService } from 'app/services/dialog.service';
+import { MessageService } from 'app/services/message.service';
+import { SpinnerService } from 'app/services/spinner.service';
+import { CarButtonAction } from 'app/types/Car';
+import { RestResponse } from 'app/types/GlobalType';
+import { ButtonColor, ButtonType, TableActionDef } from 'app/types/Table';
+import { Utils } from 'app/utils/Utils';
+import { Observable } from 'rxjs';
+import { TableAction } from './table-action';
+
+export class TableSyncRefundAction implements TableAction {
+  private action: TableActionDef = {
+    id: CarButtonAction.SYNCHRONIZE,
+    type: 'button',
+    icon: 'sync',
+    color: ButtonColor.PRIMARY,
+    name: 'general.synchronize',
+    tooltip: 'general.synchronize',
+    action: this.synchronizeRefund,
+  };
+
+  private synchronizeRefund(dialogService: DialogService, translateService: TranslateService, messageService: MessageService,
+      centralServerService: CentralServerService, spinnerService: SpinnerService, router: Router, refresh: () => Observable<void>) {
+    dialogService.createAndShowYesNoDialog(
+      translateService.instant('settings.refund.synchronize_dialog_refund_title'),
+      translateService.instant('settings.refund.synchronize_dialog_refund_confirm'),
+    ).subscribe((response) => {
+      if (response === ButtonType.YES) {
+        messageService.showInfoMessage('settings.refund.synchronize_started');
+        centralServerService.synchronizeRefundedTransactions().subscribe((synchronizeResponse) => {
+          if (synchronizeResponse.status === RestResponse.SUCCESS) {
+            messageService.showSuccessMessage('settings.refund.synchronize_success');
+            refresh().subscribe();
+          } else {
+            Utils.handleError(JSON.stringify(synchronizeResponse), messageService, 'settings.refund.synchronize_error');
+          }
+        }, (error) => {
+          Utils.handleHttpError(error, router, messageService, centralServerService,
+            'settings.refund.synchronize_error');
+        });
+      }
+    });
+  }
+
+  // Return an action
+  public getActionDef(): TableActionDef {
+    return this.action;
+  }
+}

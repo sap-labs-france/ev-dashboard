bf37339e483a51394d73b057d626c5f4fd231126
Jérôme Benoit <jerome.benoit@sap.com>
2020-05-20 12:56:00 +0200

    Socket.io: debug log for the issues' main root cause
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/app/services/central-server-notification.service.ts
+++ src/app/services/central-server-notification.service.ts
@@ -351,10 +351,12 @@ export class CentralServerNotificationService {
   }
 
   public resetSocketIO() {
-    // Check: Socket IO not initialized and user logged
+    // Check: Socket IO not initialized and user logged in
     if (this.socketIOClient) {
       // Close
       this.socketIOClient.disconnect();
+    } else {
+      console.log('Socket IO not initialized and user logged in');
     }
     // Clear
     this.socketIOClient = null;

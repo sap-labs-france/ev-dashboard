aedecf21fadfb756c0de083c010f92c2b0ff6e76
FABIANO <sergefabiano@free.fr>
2020-05-31 23:26:31 +0200

    Small renaming

--- src/app/shared/component/consumption-chart/chart-unit-selector.component.ts
+++ src/app/shared/component/consumption-chart/chart-unit-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+
 import { ConsumptionUnit } from 'app/types/Transaction';
 
 export interface UnitButtonGroup {
@@ -12,24 +13,22 @@ export interface UnitButtonGroup {
   templateUrl: './chart-unit-selector.component.html',
 })
 export class ChartUnitSelectorComponent {
+  @Output() public unitChanged = new EventEmitter();
 
   public unitMap: UnitButtonGroup[] = [
     { key: ConsumptionUnit.KILOWATT, description: 'transactions.graph.unit_kilowatts', inactive: false },
     { key: ConsumptionUnit.AMPERE, description: 'transactions.graph.unit_amperage', inactive: false }
   ];
-  @Output() public unitButtonOfScopeGroup = new EventEmitter();
-
 
   public activeUnitButtonGroup: UnitButtonGroup = this.unitMap[0];
 
-
-  public unitChanged(key: ConsumptionUnit) {
+  public buttonUnitChanged(key: ConsumptionUnit) {
     const index = this.unitMap.findIndex((element) => element.key === key);
     if (index >= 0 &&
       this.activeUnitButtonGroup.key !== key &&
       this.unitMap[index].inactive === false) {
       this.activeUnitButtonGroup = this.unitMap[index];
     }
-    this.unitButtonOfScopeGroup.emit(key);
+    this.unitChanged.emit(key);
   }
 }

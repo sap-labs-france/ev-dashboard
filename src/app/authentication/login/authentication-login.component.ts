077e35c28469dcb96609b30170ae81a88f4980ef
FABIANO <sergefabiano@free.fr>
2020-06-10 23:20:03 +0200

    Handle HTTP error code when login

--- src/app/authentication/login/authentication-login.component.ts
+++ src/app/authentication/login/authentication-login.component.ts
@@ -1,19 +1,19 @@
-import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
-import { WindowService } from 'app/services/window.service';
-import { HTTPError } from 'app/types/HTTPError';
-import { ButtonType } from 'app/types/Table';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 
 import { AuthorizationService } from '../../services/authorization.service';
+import { ButtonType } from 'app/types/Table';
 import { CentralServerService } from '../../services/central-server.service';
 import { DialogService } from '../../services/dialog.service';
+import { HTTPError } from 'app/types/HTTPError';
+import { MatDialog } from '@angular/material/dialog';
 import { MessageService } from '../../services/message.service';
 import { SpinnerService } from '../../services/spinner.service';
+import { TranslateService } from '@ngx-translate/core';
 import { Users } from '../../utils/Users';
 import { Utils } from '../../utils/Utils';
+import { WindowService } from 'app/services/window.service';
 
 declare var $: any;
 
@@ -153,15 +153,19 @@ export class AuthenticationLoginComponent implements OnInit, OnDestroy {
           this.messageService.showErrorMessage(this.messages['wrong_email_or_password']);
           break;
         // Account is locked
-        case 570:
+        case HTTPError.USER_ACCOUNT_LOCKED_ERROR:
           this.messageService.showErrorMessage(this.messages['account_locked']);
           break;
+        // Account is inactive
+        case HTTPError.USER_ACCOUNT_INACTIVE_ERROR:
+          this.messageService.showErrorMessage(this.messages['account_inactive']);
+          break;
         // Account Suspended
-        case 580:
+        case HTTPError.USER_ACCOUNT_BLOCKED_ERROR:
           this.messageService.showErrorMessage(this.messages['account_suspended']);
           break;
         // Account Pending
-        case 590:
+        case HTTPError.USER_ACCOUNT_PENDING_ERROR:
           // Pending Users from the Super Tenant should not be able to request an activation email
           if (this.subDomain !== '') {
             // Usual Users

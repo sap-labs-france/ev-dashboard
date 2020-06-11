9daf05f78486dd80f06c30c5c8aca158eadd31a8
FABIANO <sergefabiano@free.fr>
2020-06-12 00:13:52 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -30,6 +30,7 @@ export class ReleaseNotesComponent {
           {
             name: 'Dashboard',
             changes: [
+              `Added Legrand Charging Station model 059004 in the template`,
               `Push all the non zero limit Charging Plan schedules provided by the SAP Smart Charging`,
               `Handling of new Meter Values in the backend: Power, Voltage, Voltage per phase, Amperage, Amperage per phase`,
               `Enabled Charging Plan for Legrand Charging Station validation`,

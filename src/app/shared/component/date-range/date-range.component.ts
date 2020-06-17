e32d8ddbf1bfe8845351a9a32b0bf69a36b0334a
Ben Romdhane <mahdi.ben.romdhane@sap.com>
2020-06-17 09:11:04 +0200

    add datetimepicker

--- src/app/shared/component/date-range/date-range.component.ts
+++ src/app/shared/component/date-range/date-range.component.ts
@@ -1,43 +1,29 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'example-simple',
-  template: `
-            <input
-                matInput
-                ngxDaterangepickerMd
-                showCancel="true"
-                placeholder="Choose date"
-                [(ngModel)]="selected"
-                [showDropdowns]="true"
-                [lockStartDate]="false"
-                [customRangeDirection]="false"
-                (ngModelChange)="ngModelChange($event)"
-                (change)="change($event)"
-            />
-
+    selector: 'example-simple',
+    template: `
+    <input
+    matInput
+    ngxDaterangepickerMd
+    startKey="start"
+    endKey="end"
+    [locale]="{applyLabel: 'ok', format: 'DD-MM-YYYY'}"
+    formControl="formControl"
+/>
     `,
 })
 export class DateRangeComponent {
+    inlineDate: { chosenLabel: string; startDate: moment.Moment; endDate: moment.Moment };
 
-  selected = {
-    startDate: moment('2015-11-18T00:00Z'),
-    endDate: moment('2015-11-26T00:00Z'),
-  };
+    selected = {
+        startDate: moment('2015-11-18T00:00Z'),
+        endDate: moment('2015-11-26T00:00Z'),
+    };
 
-  ngModelChange(e): void {
-    console.log(e);
-  }
-
-  change(e): void {
-    console.log(e);
-  }
-
-  open(): void {
-  }
-
-  clear(e): void {
-    this.selected = null;
-  }
+    chosenDate(chosenDate: { chosenLabel: string; startDate: moment.Moment; endDate: moment.Moment }): void {
+        console.log(chosenDate);
+        this.inlineDate = chosenDate;
+    }
 }

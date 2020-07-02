964fc61268d396d40471d6f4bbb6c372dc6a18a5
FABIANO <sergefabiano@free.fr>
2020-07-02 23:39:51 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -30,6 +30,7 @@ export class ReleaseNotesComponent {
           {
             name: 'Dashboard',
             changes: [
+              `Charging Station's total power not updated when not belonging to the templates`,
               `Fixed display power with wrong unit in consumption chart`,
               `Fixed badge is no longer mandatory in user's profile`,
               `Removed Charging Station from the Gireve when changing it to private`,

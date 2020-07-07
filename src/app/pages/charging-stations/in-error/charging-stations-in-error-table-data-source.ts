2a7dc91588e33638617d18b14980a4041283fa50
mahdibenromdhane <mahdi.ben.romdhane@sap.com>
2020-07-07 16:27:47 +0200

    logs redirect refactor

--- src/app/pages/charging-stations/in-error/charging-stations-in-error-table-data-source.ts
+++ src/app/pages/charging-stations/in-error/charging-stations-in-error-table-data-source.ts
@@ -214,7 +214,7 @@ export class ChargingStationsInErrorTableDataSource extends TableDataSource<Char
         }
         break;
       case LogButtonAction.CHECK_LOGS:
-        new TableCheckLogsAction().getActionDef().action('logs?search=' + chargingStation.id);
+        this.checkLogsAction.action('logs?search=' + chargingStation.id);
         break;
     }
   }

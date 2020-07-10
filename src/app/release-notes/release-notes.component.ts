50b099f3a806176824c8b22ca8b8e76266b29aec
FABIANO <sergefabiano@free.fr>
2020-07-10 17:11:05 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -30,6 +30,7 @@ export class ReleaseNotesComponent {
           {
             name: 'Dashboard',
             changes: [
+              `Added new firmware versions in Charging Station templates`,
               `Fixed Charging Station cannot change limits when not assigned to a Site Area`,
               `Fixed add a single phase Charging Station failed on a single phase Site Area`,
               `Updated Car VIN validator`,

b3e16b8b2e8ebc6165f0dbfddb6c12b8035038bc
FABIANO <sergefabiano@free.fr>
2020-07-18 18:48:35 +0200

    Fixed build error

--- src/app/pages/cars/cell-components/car-catalog-image-formatter-cell.component.ts
+++ src/app/pages/cars/cell-components/car-catalog-image-formatter-cell.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CellContentTemplateDirective } from 'app/shared/table/cell-content-template/cell-content-template.directive';
-import { CarCatalog } from 'app/types/Car';
+import { Car, CarCatalog } from 'app/types/Car';
 
 @Component({
   template: `
@@ -12,5 +12,5 @@ import { CarCatalog } from 'app/types/Car';
 })
 
 export class CarCatalogImageFormatterCellComponent extends CellContentTemplateDirective {
-  @Input() public row!: CarCatalog;
+  @Input() public row!: CarCatalog|Car;
 }

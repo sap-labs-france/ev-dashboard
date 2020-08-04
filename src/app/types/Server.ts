37f46ab55cea16f1098bbf86e0d1c97ce994b9a9
System Administrator <sergefabiano@free.fr>
2020-08-04 16:07:27 +0200

    Updated server actions

--- src/app/types/Server.ts
+++ src/app/types/Server.ts
@@ -88,6 +88,7 @@ export enum ServerAction {
 
   REMOTE_PUSH_NOTIFICATION = 'RemotePushNotification',
   EMAIL_NOTIFICATION = 'EmailNotification',
+  END_USER_ERROR_NOTIFICATION = 'EndUserErrorNotification',
 
   SYNCHRONIZE_REFUND = 'RefundSynchronize',
 
@@ -137,6 +138,8 @@ export enum ServerAction {
   OCPI_GET_SESSIONS = 'OcpiGetSessions',
   OCPI_GET_TOKENS = 'OcpiGetTokens',
   OCPI_GET_CDRS = 'OcpiGetCdrs',
+  OCPI_GET_TARIFF = 'OcpiGetTariff',
+  OCPI_GET_TARIFFS = 'OcpiGetTariffs',
   OCPI_POST_CREDENTIALS = 'OcpiPostCredentials',
   OCPI_DELETE_CREDENTIALS = 'OcpiDeleteCredentials',
   OCPI_ENDPOINT_UPDATE = 'OcpiEndpointUpdate',
@@ -353,4 +356,7 @@ export enum ServerAction {
   SMART_CHARGING = 'SmartCharging',
 
   INSTANTIATE_DUMMY_MODULE = 'InstantiateDummyModule',
+
+  AXIOS_REQUEST = 'AxiosRequest',
+  AXIOS_RESPONSE = 'AxiosResponse',
 }

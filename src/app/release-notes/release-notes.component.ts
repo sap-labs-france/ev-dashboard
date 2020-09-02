68350f87639920819c193c858fcd517ff39fba95
FABIANO <sergefabiano@free.fr>
2020-09-02 11:21:25 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -31,7 +31,7 @@ export class ReleaseNotesComponent {
             name: 'Dashboard',
             changes: [
               `Display Asset dynamic consumption`,
-              `Don't take into accouont Inactivity when a Charging Plan is lowering the power below 13 Amps per phase`,
+              `Don't take into account Inactivity when a Charging Plan is lowering the power below 13 Amps per phase`,
               `Send End of Charge Notification when the last 5 mins consumption is 0 kW.h`,
               `Added Atess Charging Station vendor in templates`,
               `Add a new field Amps per phase in Edit Charging Station`,

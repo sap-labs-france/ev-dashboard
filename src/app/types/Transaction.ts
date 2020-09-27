90d198e33eeccbb7706a94bfb46c1c7ea6d7e41c
Jérôme Benoit <jerome.benoit@sap.com>
2020-09-27 22:23:50 +0200

    Avoid backend connection retry in case of authentication error.
    And make a proper usage of http-status-codes module.
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/app/types/Transaction.ts
+++ src/app/types/Transaction.ts
@@ -1,10 +1,10 @@
 import { BillingTransactionData } from './Billing';
 import { ChargingStation } from './ChargingStation';
-import { OCPICdr } from './ocpi/OCPICdr';
-import { OCPISession } from './ocpi/OCPISession';
 import { RefundStatus, RefundType } from './Refund';
 import { Data } from './Table';
 import { User } from './User';
+import { OCPICdr } from './ocpi/OCPICdr';
+import { OCPISession } from './ocpi/OCPISession';
 
 export interface Transaction extends Data {
   id: number;

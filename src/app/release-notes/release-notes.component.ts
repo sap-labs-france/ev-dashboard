c49d05ff7c19d5a8be5b84bd4df8a45860ae3225
FABIANO <sergefabiano@free.fr>
2020-09-29 21:26:51 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -32,6 +32,7 @@ export class ReleaseNotesComponent {
             changes: [
               `Allow high volume of data in export functions`,
               `For Badge ID to be in upper case when created manually`,
+              `Build Smart Charging safe Car + Override with Meter Values`,
               `Enable vehicule identifier in OCPP for the Ebee Charging Station`,
               `Handle Meter Values with L1_N, L2_N and L3_N phases`,
               `Add Voltage Meter Value on Legrand Charging Station`,

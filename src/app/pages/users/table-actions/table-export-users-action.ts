abf13205fb010f22af66dbe60911dc2cb1c1891d
WillyLellouch <willy.lellouch@gmail.com>
2020-10-15 09:53:54 +0200

    refactoring of functionality

--- src/app/pages/users/table-actions/table-export-users-action.ts
+++ src/app/pages/users/table-actions/table-export-users-action.ts
@@ -1,40 +1,37 @@
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { CentralServerService } from 'app/services/central-server.service';
 import { DialogService } from 'app/services/dialog.service';
+import { MessageService } from 'app/services/message.service';
+import { SpinnerService } from 'app/services/spinner.service';
 import { TableExportAction } from 'app/shared/table/actions/table-export-action';
-import { ButtonType, TableActionDef } from 'app/types/Table';
-import { User, UserButtonAction } from 'app/types/User';
-import { Constants } from 'app/utils/Constants';
+import { FilterParams } from 'app/types/GlobalType';
+import { TableActionDef } from 'app/types/Table';
+import { UserButtonAction } from 'app/types/User';
+
+export interface TableExportUsersActionDef extends TableActionDef {
+  action: (filters: FilterParams, dialogService: DialogService, translateService: TranslateService,
+    messageService: MessageService, centralServerService: CentralServerService, router: Router,
+    spinnerService: SpinnerService) => void;
+}
 
 export class TableExportUsersAction extends TableExportAction {
-  public getActionDef(): TableActionDef {
+  public getActionDef(): TableExportUsersActionDef {
     return {
       ...super.getActionDef(),
       id: UserButtonAction.EXPORT_USERS,
-      name: 'general.export',
-      action: this.exportUsersAsCSV,
+      action: this.exportUsers,
     };
   }
 
-  public exportUsersAsCSV(users: User[], dialogService: DialogService, translateService: TranslateService) {
-    dialogService.createAndShowYesNoDialog(
-      translateService.instant('users.export_users_title'),
-      translateService.instant('users.export_users_confirm'),
-    ).subscribe((response) => {
-      if (response === ButtonType.YES) {
-        let csv = `Name${Constants.CSV_SEPARATOR}First Name${Constants.CSV_SEPARATOR}Role${Constants.CSV_SEPARATOR}Status${Constants.CSV_SEPARATOR}Email${Constants.CSV_SEPARATOR}Badges${Constants.CSV_SEPARATOR}EULA Accepted On${Constants.CSV_SEPARATOR}Created On${Constants.CSV_SEPARATOR}Changed On${Constants.CSV_SEPARATOR}Changed By\r\n`;
-        for (const user of users) {
-          const tags = [];
-          for (const tag of user.tags) {
-            tags.push(tag.id);
-          }
-          const tagsString = tags.toString();
-          csv += `${user.name}${Constants.CSV_SEPARATOR}${user.name}${Constants.CSV_SEPARATOR}${user.role}${Constants.CSV_SEPARATOR}${user.status}${Constants.CSV_SEPARATOR}${user.email}${Constants.CSV_SEPARATOR}${tagsString}${Constants.CSV_SEPARATOR}${user.eulaAcceptedOn}${Constants.CSV_SEPARATOR}${user.createdOn}${Constants.CSV_SEPARATOR}${user.lastChangedOn}${Constants.CSV_SEPARATOR}${user.lastChangedBy}\r\n`;
-        }
-        const blob = new Blob([csv]);
-        saveAs(blob, `exported-users.csv`);
-      }
-    });
+  private exportUsers(filters: FilterParams, dialogService: DialogService, translateService: TranslateService,
+    messageService: MessageService, centralServerService: CentralServerService, router: Router,
+    spinnerService: SpinnerService) {
+    super.export(filters, 'exported-users.csv',
+      'users.export_users_title', 'users.export_users_confirm',
+      'users.export_users_error',
+      centralServerService.exportUsers.bind(centralServerService),
+      dialogService, translateService, messageService,
+      centralServerService, spinnerService, router);
   }
 }
-
-// "${Utils.replaceSpecialCharsInCSVValueParam(parameter.value)}"

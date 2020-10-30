528e0d789a2612ccdec0941d1e3f7a1a07989471
LittlePinkCookie <alouache.loan@gmail.com>
2020-10-30 12:14:20 +0100

    Changed sorting format

--- src/app/services/central-server.service.ts
+++ src/app/services/central-server.service.ts
@@ -8,6 +8,7 @@ import { Car, CarCatalog, CarMaker, ImageObject } from 'app/types/Car';
 import { ChargingProfile, GetCompositeScheduleCommandResult } from 'app/types/ChargingProfile';
 import { ChargePoint, ChargingStation, OCPPAvailabilityType, OcppParameter } from 'app/types/ChargingStation';
 import { Company } from 'app/types/Company';
+import CentralSystemServerConfiguration from 'app/types/configuration/CentralSystemServerConfiguration';
 import { IntegrationConnection, UserConnection } from 'app/types/Connection';
 import { ActionResponse, ActionsResponse, CheckAssetConnectionResponse, CheckBillingConnectionResponse, DataResult, LoginResponse, OCPIGenerateLocalTokenResponse, OCPIJobStatusesResponse, OCPIPingResponse, OCPITriggerJobsResponse, Ordering, Paging } from 'app/types/DataResult';
 import { EndUserLicenseAgreement } from 'app/types/Eula';
@@ -15,6 +16,8 @@ import { FilterParams, Image, KeyValue, Logo } from 'app/types/GlobalType';
 import { HTTPAuthError, HTTPError } from 'app/types/HTTPError';
 import { AssetInError, ChargingStationInError, TransactionInError } from 'app/types/InError';
 import { Log } from 'app/types/Log';
+import { OcpiEndpoint } from 'app/types/ocpi/OCPIEndpoint';
+import { OCPPResetType } from 'app/types/ocpp/OCPP';
 import { RefundReport } from 'app/types/Refund';
 import { RegistrationToken } from 'app/types/RegistrationToken';
 import { ServerAction } from 'app/types/Server';
@@ -26,9 +29,6 @@ import { Tag } from 'app/types/Tag';
 import { Tenant } from 'app/types/Tenant';
 import { Transaction } from 'app/types/Transaction';
 import { User, UserCar, UserSite, UserToken } from 'app/types/User';
-import CentralSystemServerConfiguration from 'app/types/configuration/CentralSystemServerConfiguration';
-import { OcpiEndpoint } from 'app/types/ocpi/OCPIEndpoint';
-import { OCPPResetType } from 'app/types/ocpp/OCPP';
 import { Utils } from 'app/utils/Utils';
 import { StatusCodes } from 'http-status-codes';
 import { BehaviorSubject, EMPTY, Observable, TimeoutError, throwError, timer } from 'rxjs';
@@ -3087,16 +3087,13 @@ export class CentralServerService {
     // Check
     if (ordering && ordering.length) {
       const sortFields: string[] = [];
-      const sortDirs: string[] = [];
       for (const order of ordering) {
         if (order.field) {
           sortFields.push(order.field);
-          sortDirs.push(order.direction);
         }
       }
       if (sortFields.length > 0) {
         queryParams['SortFields'] = sortFields;
-        queryParams['SortDirs'] = sortDirs;
       }
     }
   }

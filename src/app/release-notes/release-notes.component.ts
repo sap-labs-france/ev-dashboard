11b2c7e536fa6a7fc60c35375a14cf34f05d4fca
FABIANO <sergefabiano@free.fr>
2020-11-08 13:40:15 +0100

    Updated release-notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -31,10 +31,10 @@ export class ReleaseNotesComponent {
             name: 'Dashboard',
             changes: [
               `Hide table list actions for OCPI entities`,
-              `Push all the locations once a day`,
+              `Push all the OCPI locations once a day`,
               `Retrieve all the OCPI badges once a day`,
               `Notify the user when a new invoice is available`,
-              `Fixed billing invoice deletion during synchronization`,
+              `Fixed billing invoice deletion during invoice synchronization`,
               `Added Car Maker filter in Car Management`,
               `Jump from User to Badge list should take into account the organization filter`,
               `Format OCPI Country codes`,

a53b6b30afd2b48f4fb3510e6f8377e3bfde0877
FABIANO <sergefabiano@free.fr>
2020-11-15 13:35:19 +0100

    Renamed ev-dashboard-new to ev-dashboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -20,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ev-dashboard-new'`, () => {
+  it(`should have as title 'ev-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ev-dashboard-new');
+    expect(app.title).toEqual('ev-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ev-dashboard-new app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ev-dashboard app is running!');
   });
 });

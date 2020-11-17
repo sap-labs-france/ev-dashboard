d848758fe9bb3b2fea86d39b53916eb7a76ff081
Jérôme Benoit <jerome.benoit@sap.com>
2020-11-17 21:24:40 +0100

    socket.io: initial portage to 3.x version
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/app/utils/SocketIOClient.ts
+++ src/app/utils/SocketIOClient.ts
@@ -1,10 +1,10 @@
-import * as io from 'socket.io-client';
+import { Manager, Socket } from 'socket.io-client';
 
 import { Utils } from './Utils';
 
 export default class SocketIOClient {
   private static instance: SocketIOClient;
-  private socketIO: SocketIOClient.Socket;
+  private socketIO: Socket;
 
   private constructor() {
   }
@@ -26,30 +26,30 @@ export default class SocketIOClient {
   public connectAuthenticated(serverURL: string, token: string, connectCallback: () => void = () => { }) {
     // Check
     if (!this.socketIO && serverURL && token) {
-      // Init and connect Socket IO
-      this.socketIO = io(serverURL, {
+      // Init and connect Socket IO client
+      const manager = new Manager(serverURL, {
         query: 'token=' + token,
         transports: ['websocket'],
       });
+      this.socketIO = manager.socket('/');
     } else if (this.socketIO && this.socketIO.disconnected) {
       // Connect Socket IO
       this.socketIO.connect();
     } else {
-      // Utils.consoleDebugLog('Missing serverURL and token arguments');
+      Utils.consoleDebugLog('SocketIO client connection: missing serverURL and token arguments');
     }
+    this.socketIO.on('connect', () => {
+      Utils.consoleDebugLog(`SocketIO client is connected`);
+      connectCallback();
+    });
+    this.socketIO.on('authenticated', () => {
+      Utils.consoleDebugLog('SocketIO client token authenticated');
+    });
     this.socketIO.on('unauthorized', (error) => {
       if (error.data.type === 'UnauthorizedError' || error.data.code === 'invalid_token') {
         // Redirect user to login page perhaps?
         Utils.consoleDebugLog('SocketIO client user token has expired');
       }
-    });
-    this.socketIO.on('connect', () => {
-      Utils.consoleDebugLog(`SocketIO client is connected`);
-      connectCallback();
-    });
-    // On reconnection, reset the transports option
-    this.socketIO.on('reconnect_attempt', () => {
-      this.socketIO.io.opts.transports = ['polling', 'websocket'];
     });
     this.socketIO.on('connect_timeout', (timeout) => { Utils.consoleDebugLog(`SocketIO client connection timeout: ${timeout}`); });
     this.socketIO.on('connect_error', (error) => { Utils.consoleDebugLog(`SocketIO client connect error: ${error}`); });

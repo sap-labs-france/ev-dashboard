cd9172f381393db7da98013bf1b8f37ebb29e48a
Christopher Boehm <christopher.boehm@sap.com>
2020-12-01 13:35:16 +0100

    try copy config files

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -1,21 +1,25 @@
 import static com.sap.piper.internal.Prerequisites.checkScript
 
 void call(Map params) {
+	node{
+	echo "do acceptance stage"
+	echo "Cloning deployment config repository.."
+	checkout([
+		$class: 'GitSCM',
+		extensions: [
+			[$class: 'RelativeTargetDirectory', relativeTargetDir: "deployment"],
+		],
+		userRemoteConfigs: [
+			[credentialsId: 'GitHub-emobility-ci-build', url: "https://github.wdf.sap.corp/EVSE/ev-scp-sap-it.git"]
+		]
+		])
+	sh "ls"
+	sh "ls deployment"
+	sh "cp -pr ./deployment/e-Mobility-dev/sap-ev-front-end-server/* ."
+	sh "ls"
+	sh "ls src/assets"
 
-  echo "do acceptance stage"
-	echo "Cloning Helm chart repository.."
-  checkout([
-    $class: 'GitSCM',
-    extensions: [
-        [$class: 'RelativeTargetDirectory', relativeTargetDir: "deployment"],
-    ],
-    userRemoteConfigs: [
-        [credentialsId: 'GitHub-emobility-ci-build', url: "https://github.wdf.sap.corp/EVSE/ev-scp-sap-it.git"]
-    ]
-  ])
-  sh "ls"
-  sh "ls deployment"
-  //params.originalStage()
+	//params.originalStage()
 
 }
 return this

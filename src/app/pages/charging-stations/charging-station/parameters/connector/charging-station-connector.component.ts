462ea16b5c11e682c03b9a67f054a64789a8adc8
FABIANO <sergefabiano@free.fr>
2020-12-05 01:08:55 +0100

    Base phase order on number of phase in charging station

--- src/app/pages/charging-stations/charging-station/parameters/connector/charging-station-connector.component.ts
+++ src/app/pages/charging-stations/charging-station/parameters/connector/charging-station-connector.component.ts
@@ -148,6 +148,8 @@ export class ChargingStationConnectorComponent implements OnInit, OnChanges {
         Utils.getNumberOfConnectedPhases(this.chargingStation, chargePoint, this.connector.connectorId));
       if (this.numberOfConnectedPhase.value === 1) {
         this.phaseAssignmentToGridMap = this.phaseAssignmentToGridMapSinglePhased;
+      } else {
+        this.phaseAssignmentToGridMap = this.phaseAssignmentToGridMapThreePhased;
       }
       if (this.connector.phaseAssignmentToGrid) {
         this.phaseAssignmentToGrid.setValue(this.phaseAssignmentToGridMap[
@@ -190,6 +192,11 @@ export class ChargingStationConnectorComponent implements OnInit, OnChanges {
     } else {
       this.numberOfConnectedPhase.enable();
       this.phaseAssignmentToGrid.enable();
+      if (this.numberOfConnectedPhase.value === 1) {
+        this.phaseAssignmentToGridMap = this.phaseAssignmentToGridMapSinglePhased;
+      } else {
+        this.phaseAssignmentToGridMap = this.phaseAssignmentToGridMapThreePhased;
+      }
     }
   }
 

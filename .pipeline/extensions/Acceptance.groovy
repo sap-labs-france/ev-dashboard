c74f0c7353b0229c20cf419eb10fb9b6b9b085d2
Christopher Boehm <christopher.boehm@sap.com>
2020-12-14 09:24:22 +0100

    skip stage for now

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -2,37 +2,33 @@ import static com.sap.piper.internal.Prerequisites.checkScript
 
 void call(Map params) {
 
-	echo "do acceptance stage"
+	echo "skip acceptance stage for now"
 	// sh "ls"
-	deleteDir()
+	//deleteDir()
 	//sh "ls"
-	//unstash 'deployDescriptor'
-	sh "ls"
 	//echo "checkout dashboard source"
-	checkout scm
-	sh "ls"
-	echo "Cloning deployment config repository.."
-	checkout([
-		$class: 'GitSCM',
-		extensions: [
-			[$class: 'RelativeTargetDirectory', relativeTargetDir: "deployment"],
-		],
-		userRemoteConfigs: [
-			[credentialsId: 'GitHub-emobility-ci-build', url: "https://github.wdf.sap.corp/EVSE/ev-scp-sap-it.git"]
-		]
-		])
-	sh "ls"
-	sh "ls deployment"
-	sh "cp -pr ./deployment/e-Mobility-dev/sap-ev-front-end-server/* ."
-	sh "ls"
-	sh "pwd"
-	sh "find . -name 'manifest*'"
-	sh "find . -name 'config.json'"
-	sh "ls src/assets"
-	//unstash 'deployDescriptor'
-	//stash 'deployDescriptor'
+	//checkout scm
+	//sh "ls"
+	//echo "Cloning deployment config repository.."
+	//checkout([
+	//	$class: 'GitSCM',
+	//	extensions: [
+	//		[$class: 'RelativeTargetDirectory', relativeTargetDir: "deployment"],
+	//	],
+	//	userRemoteConfigs: [
+	//		[credentialsId: 'GitHub-emobility-ci-build', url: "https://github.wdf.sap.corp/EVSE/ev-scp-sap-it.git"]
+	//	]
+	//	])
+	//sh "ls"
+	//sh "ls deployment"
+	//sh "cp -pr ./deployment/e-Mobility-dev/sap-ev-front-end-server/* ."
+	//sh "ls"
+	//sh "pwd"
+	//sh "find . -name 'manifest*'"
+	//sh "find . -name 'config.json'"
+	//sh "ls src/assets"
 
-	params.originalStage()
+	//params.originalStage()
 
 }
 return this

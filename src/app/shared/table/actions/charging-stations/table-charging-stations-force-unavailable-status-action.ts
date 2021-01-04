1593dc9c95b7cdb88c8cd5d587cccb68f41cc18c
Jérôme Benoit <jerome.benoit@sap.com>
2021-01-04 10:53:26 +0100

    Fix ChangeAvailability return handling.
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/app/shared/table/actions/charging-stations/table-charging-stations-force-unavailable-status-action.ts
+++ src/app/shared/table/actions/charging-stations/table-charging-stations-force-unavailable-status-action.ts
@@ -1,12 +1,13 @@
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
+import { OCPPAvailabilityStatus } from 'types/ocpp/OCPP';
 
 import { CentralServerService } from '../../../../services/central-server.service';
 import { DialogService } from '../../../../services/dialog.service';
 import { MessageService } from '../../../../services/message.service';
 import { SpinnerService } from '../../../../services/spinner.service';
-import { ChargingStation, ChargingStationButtonAction, OCPPGeneralResponse } from '../../../../types/ChargingStation';
+import { ChargingStation, ChargingStationButtonAction } from '../../../../types/ChargingStation';
 import { ActionResponse } from '../../../../types/DataResult';
 import { ButtonColor, ButtonType, TableActionDef } from '../../../../types/Table';
 import { Utils } from '../../../../utils/Utils';
@@ -46,7 +47,7 @@ export class TableChargingStationsForceUnavailableStatusAction implements TableA
         // Change Availability
         centralServerService.chargingStationChangeAvailability(chargingStation.id, false).subscribe((response: ActionResponse) => {
           spinnerService.hide();
-          if (response.status === OCPPGeneralResponse.ACCEPTED) {
+          if (response.status === OCPPAvailabilityStatus.ACCEPTED || response.status === OCPPAvailabilityStatus.SCHEDULED) {
             messageService.showSuccessMessage(
               translateService.instant('chargers.force_unavailable_status_success', { chargeBoxID: chargingStation.id }));
             if (refresh) {

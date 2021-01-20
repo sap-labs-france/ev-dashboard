7377983c3f908cd16396b17a29d57e080d7b0481
mahdibenromdhane <mahdi.benromdhane@esprit.tn>
2021-01-20 12:40:01 +0100

    connector filter

--- src/app/pages/charging-stations/list/charging-stations-list-table-data-source.ts
+++ src/app/pages/charging-stations/list/charging-stations-list-table-data-source.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
+import { ConnectorTableFilter } from 'shared/table/filters/connector-table-filter';
 
 import { AuthorizationService } from '../../../services/authorization.service';
 import { CentralServerNotificationService } from '../../../services/central-server-notification.service';
@@ -329,6 +330,7 @@ export class ChargingStationsListTableDataSource extends TableDataSource<Chargin
         new IssuerFilter().getFilterDef(),
         new SiteTableFilter().getFilterDef(),
         new SiteAreaTableFilter().getFilterDef(),
+        new ConnectorTableFilter().getFilterDef()
       ];
     }
     return [];
---
+++ src/app/shared/model/charging-stations.model.ts
@@ -0,0 +1,31 @@
+import { KeyValue } from 'types/GlobalType';
+import { Utils } from 'utils/Utils';
+
+export const CONNECTORS: KeyValue[] = [
+  { key: '1', value: Utils.getConnectorLetterFromConnectorID(1) },
+  { key: '2', value: Utils.getConnectorLetterFromConnectorID(2) },
+  { key: '3', value: Utils.getConnectorLetterFromConnectorID(3) },
+  { key: '4', value: Utils.getConnectorLetterFromConnectorID(4) },
+  { key: '5', value: Utils.getConnectorLetterFromConnectorID(5) },
+  { key: '6', value: Utils.getConnectorLetterFromConnectorID(6) },
+  { key: '7', value: Utils.getConnectorLetterFromConnectorID(7) },
+  { key: '8', value: Utils.getConnectorLetterFromConnectorID(8) },
+  { key: '9', value: Utils.getConnectorLetterFromConnectorID(9) },
+  { key: '10', value: Utils.getConnectorLetterFromConnectorID(10) },
+  { key: '11', value: Utils.getConnectorLetterFromConnectorID(11) },
+  { key: '12', value: Utils.getConnectorLetterFromConnectorID(12) },
+  { key: '13', value: Utils.getConnectorLetterFromConnectorID(13) },
+  { key: '14', value: Utils.getConnectorLetterFromConnectorID(14) },
+  { key: '15', value: Utils.getConnectorLetterFromConnectorID(15) },
+  { key: '16', value: Utils.getConnectorLetterFromConnectorID(16) },
+  { key: '17', value: Utils.getConnectorLetterFromConnectorID(17) },
+  { key: '18', value: Utils.getConnectorLetterFromConnectorID(18) },
+  { key: '19', value: Utils.getConnectorLetterFromConnectorID(19) },
+  { key: '20', value: Utils.getConnectorLetterFromConnectorID(20) },
+  { key: '21', value: Utils.getConnectorLetterFromConnectorID(21) },
+  { key: '22', value: Utils.getConnectorLetterFromConnectorID(22) },
+  { key: '23', value: Utils.getConnectorLetterFromConnectorID(23) },
+  { key: '24', value: Utils.getConnectorLetterFromConnectorID(24) },
+  { key: '25', value: Utils.getConnectorLetterFromConnectorID(25) },
+  { key: '26', value: Utils.getConnectorLetterFromConnectorID(26) }
+];

c9d5097d180770b3435a9ae19f37bcebf5b5f703
OliveGerste <33114317+OliveGerste@users.noreply.github.com>
2021-02-02 16:45:05 +0100

    filter issuer + show positve values for prod-asset

---
+++ src/app/pages/assets/cell-components/asset-consumption-cell.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Asset, AssetType } from 'types/Asset';
+
+import { CellContentTemplateDirective } from '../../../shared/table/cell-content-template/cell-content-template.directive';
+
+@Component({
+  template: `
+    <span>
+      <ng-container>
+        <span class="ml-1">
+          {{row.currentInstantWatts || row.currentInstantWatts === 0 ? ((row.assetType === assetTypeProduction ? row.currentInstantWatts * -1 : row.currentInstantWatts) | appUnit:'W':'kW') : '-'}}
+        </span>
+      </ng-container>
+    </span>
+  `,
+})
+export class AssetConsumptionCellComponent extends CellContentTemplateDirective {
+  @Input() public row!: Asset;
+  public assetTypeProduction = AssetType.PRODUCTION;
+}

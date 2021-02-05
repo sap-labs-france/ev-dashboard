4534d8ce4ddbe7b6970140786459e053331fd768
Stefania Santimbrean <stefaniasantimbrean5@gmail.com>
2021-02-05 11:01:50 +0200

    handle migration in progress in server

--- src/app/pages/settings-technical/crypto/settings-crypto.component.ts
+++ src/app/pages/settings-technical/crypto/settings-crypto.component.ts
@@ -64,55 +64,54 @@ export class SettingsCryptoComponent implements OnInit {
   }
 
   public save(content: any) {
-    if (this.cryptoSettings.crypto.migrationToBeDone) {
-      this.messageService.showWarningMessage('technical_settings.crypto.migration_in_progress_warning');
-    } else {
-      if (!content.crypto) {
-        return;
-      }
-      this.cryptoSettings.type = CryptoSettingsType.CRYPTO;
-      this.cryptoSettings.crypto.formerKey = this.cryptoSettings.crypto.key;
-      this.cryptoSettings.crypto.key = content.crypto.key;
-      this.cryptoSettings.crypto.formerKeyProperties = this.cryptoSettings.crypto.keyProperties;
-      this.cryptoSettings.crypto.keyProperties = {
-        blockCypher: content.crypto.blockCypher,
-        blockSize: content.crypto.blockSize,
-        operationMode: content.crypto.operationMode,
-      };
-      // Save
-      this.spinnerService.show();
-      this.componentService.saveCryptoSettings(this.cryptoSettings).subscribe(
-        (response) => {
-          this.spinnerService.hide();
-          if (response.status === RestResponse.SUCCESS) {
-            this.messageService.showSuccessMessage('technical_settings.crypto.update_success');
-            this.refresh();
-          } else {
-            Utils.handleError(
-              JSON.stringify(response),
+    if (!content.crypto) {
+      return;
+    }
+    this.cryptoSettings.type = CryptoSettingsType.CRYPTO;
+    this.cryptoSettings.crypto.formerKey = this.cryptoSettings.crypto.key;
+    this.cryptoSettings.crypto.key = content.crypto.key;
+    this.cryptoSettings.crypto.formerKeyProperties = this.cryptoSettings.crypto.keyProperties;
+    this.cryptoSettings.crypto.keyProperties = {
+      blockCypher: content.crypto.blockCypher,
+      blockSize: content.crypto.blockSize,
+      operationMode: content.crypto.operationMode,
+    };
+    // Save
+    this.spinnerService.show();
+    this.componentService.saveCryptoSettings(this.cryptoSettings).subscribe(
+      (response) => {
+        this.spinnerService.hide();
+        if (response.status === RestResponse.SUCCESS) {
+          this.messageService.showSuccessMessage('technical_settings.crypto.update_success');
+          this.refresh();
+        } else {
+          Utils.handleError(
+            JSON.stringify(response),
+            this.messageService,
+            'technical_settings.crypto.update_error'
+          );
+        }
+      },
+      (error) => {
+        this.spinnerService.hide();
+        switch (error.status) {
+          case HTTPError.OBJECT_DOES_NOT_EXIST_ERROR:
+            this.messageService.showErrorMessage('technical_settings.crypto.setting_do_not_exist');
+            break;
+          case HTTPError.MIGRATION_IN_PROGRESS:
+            this.messageService.showErrorMessage('technical_settings.crypto.migration_in_progress_error');
+            break;
+          default:
+            Utils.handleHttpError(
+              error,
+              this.router,
               this.messageService,
+              this.centralServerService,
               'technical_settings.crypto.update_error'
             );
-          }
-        },
-        (error) => {
-          this.spinnerService.hide();
-          switch (error.status) {
-            case HTTPError.OBJECT_DOES_NOT_EXIST_ERROR:
-              this.messageService.showErrorMessage('technical_settings.crypto.setting_do_not_exist');
-              break;
-            default:
-              Utils.handleHttpError(
-                error,
-                this.router,
-                this.messageService,
-                this.centralServerService,
-                'technical_settings.crypto.update_error'
-              );
-          }
         }
-      );
-    };
+      }
+    );
   }
 
   public refresh() {

b857ca77d2094557e625941099bf75d856769253
LucasBrazi06 <sergefabiano@free.fr>
2021-02-22 15:50:20 +0100

    Fixed indentation

--- src/app/shared/table/actions/charging-stations/table-revoke-registration-token-action.ts
+++ src/app/shared/table/actions/charging-stations/table-revoke-registration-token-action.ts
@@ -11,31 +11,31 @@ import { TableActionDef } from '../../../../types/Table';
 import { TableRevokeAction } from '../table-revoke-action';
 
 export interface TableRevokeRegistrationTokenActionDef extends TableActionDef {
-    action: (registrationToken: RegistrationToken, dialogService: DialogService, translateService: TranslateService,
-        messageService: MessageService, centralServerService: CentralServerService, spinnerService: SpinnerService,
-        router: Router, refresh?: () => Observable<void>) => void;
-    }
+  action: (registrationToken: RegistrationToken, dialogService: DialogService, translateService: TranslateService,
+    messageService: MessageService, centralServerService: CentralServerService, spinnerService: SpinnerService,
+    router: Router, refresh?: () => Observable<void>) => void;
+  }
 
 export class TableRevokeRegistrationTokenAction extends TableRevokeAction {
-    public getActionDef(): TableRevokeRegistrationTokenActionDef {
-        return {
-            ...super.getActionDef(),
-            id: RegistrationTokenButtonAction.REVOKE_TOKEN,
-            action: this.revokeRegistrationToken,
-        };
-    }
+  public getActionDef(): TableRevokeRegistrationTokenActionDef {
+    return {
+      ...super.getActionDef(),
+      id: RegistrationTokenButtonAction.REVOKE_TOKEN,
+      action: this.revokeRegistrationToken,
+    };
+  }
 
-    private revokeRegistrationToken(registrationToken: RegistrationToken, dialogService: DialogService,
-        translateService: TranslateService, messageService: MessageService,
-        centralServerService: CentralServerService, spinnerService: SpinnerService, router: Router, refresh?: () => Observable<void>) {
-        super.revoke(
-            registrationToken,
-            translateService.instant('chargers.connections.registration_token_revoke_title'),
-            translateService.instant('chargers.connections.registration_token_revoke_confirm'),
-            translateService.instant('chargers.connections.registration_token_revoke_success'),
-            translateService.instant('chargers.connections.registration_token_revoke_error'),
-            centralServerService.revokeRegistrationToken.bind(centralServerService),
-            dialogService, translateService, messageService, centralServerService, spinnerService, router, refresh
-        );
-    }
+  private revokeRegistrationToken(registrationToken: RegistrationToken, dialogService: DialogService,
+    translateService: TranslateService, messageService: MessageService,
+    centralServerService: CentralServerService, spinnerService: SpinnerService, router: Router, refresh?: () => Observable<void>) {
+    super.revoke(
+      registrationToken,
+      translateService.instant('chargers.connections.registration_token_revoke_title'),
+      translateService.instant('chargers.connections.registration_token_revoke_confirm'),
+      translateService.instant('chargers.connections.registration_token_revoke_success'),
+      translateService.instant('chargers.connections.registration_token_revoke_error'),
+      centralServerService.revokeRegistrationToken.bind(centralServerService),
+      dialogService, translateService, messageService, centralServerService, spinnerService, router, refresh
+    );
+  }
 }

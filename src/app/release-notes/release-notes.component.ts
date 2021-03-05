3411ec757519af2585ffc22b65a622c079d73bb9
LucasBrazi06 <sergefabiano@free.fr>
2021-03-05 09:41:38 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -24,6 +24,18 @@ export class ReleaseNotesComponent {
   public buidlReleaseNotes() {
     this.releaseNotes = [
       {
+        version: '2.4.47',
+        date: new Date('2020-03-05'),
+        componentChanges: [
+          {
+            name: 'Dashboard',
+            changes: [
+              `Added IES Wallbox g3 MonoCombo in Charging Station's template`,
+            ],
+          },
+        ],
+      },
+      {
         version: '2.4.45',
         date: new Date('2020-03-03'),
         componentChanges: [

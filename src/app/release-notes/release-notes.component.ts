b3c1f99455edf008197f940e47d673707fd04c38
LucasBrazi06 <sergefabiano@free.fr>
2021-04-01 09:49:32 +0200

    Updated version

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -25,7 +25,7 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.4.57',
-        date: new Date('2021-03-30'),
+        date: new Date('2021-04-01'),
         componentChanges: [
           {
             name: 'Dashboard',

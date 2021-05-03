977315f7be0e57ee0a2afbf83b73566c0bbc3c32
LucasBrazi06 <sergefabiano@free.fr>
2021-05-03 15:50:15 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -24,6 +24,18 @@ export class ReleaseNotesComponent {
   public buildReleaseNotes() {
     this.releaseNotes = [
       {
+        version: '2.4.65',
+        date: new Date('2021-05-03'),
+        componentChanges: [
+          {
+            name: 'Dashboard',
+            changes: [
+              `User with role Demo cannot read his profile`,
+            ],
+          },
+        ],
+      },
+      {
         version: '2.4.64',
         date: new Date('2021-05-01'),
         componentChanges: [

b005969bd6f646797d133657a36f127aafc566b5
Alfons <albrcmon@inf.upv.es>
2021-05-18 10:49:05 +0100

    string concatenation to template literals

--- src/app/pages/users/tags/tags-list-table-data-source.ts
+++ src/app/pages/users/tags/tags-list-table-data-source.ts
@@ -355,12 +355,12 @@ export class TagsListTableDataSource extends TableDataSource<Tag> {
         break;
       case UserButtonAction.NAVIGATE_TO_USER:
         if (actionDef.action) {
-          (actionDef as TableOpenURLActionDef).action('users#all?TagID=' + tag.id + '&Issuer=' + tag.issuer);
+          (actionDef as TableOpenURLActionDef).action(`users#all?TagID=${tag.id}&Issuer=${tag.issuer}`);
         }
         break;
       case TransactionButtonAction.NAVIGATE_TO_TRANSACTIONS:
         if (actionDef.action) {
-          (actionDef as TableOpenURLActionDef).action('transactions#history?TagID=' + tag.id + '&Issuer=' + tag.issuer);
+          (actionDef as TableOpenURLActionDef).action(`transactions#history?TagID=${tag.id}&Issuer=${tag.issuer}`);
         }
         break;
     }

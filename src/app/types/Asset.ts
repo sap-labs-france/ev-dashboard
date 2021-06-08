f71958518fae1fd358d3ea798ca964c8317b1acc
OliveGerste <33114317+OliveGerste@users.noreply.github.com>
2021-06-08 14:49:23 +0200

    added configuration of asset variation threshold

--- src/app/types/Asset.ts
+++ src/app/types/Asset.ts
@@ -24,6 +24,8 @@ export interface Asset extends TableData, CreatedUpdatedProps, AbstractCurrentCo
   lastChangedOn: Date;
   connected: boolean;
   excludeFromSmartCharging?: boolean;
+  variationThresholdPercent?: number;
+  powerWattsLastSmartChargingRun?: number;
 }
 
 export interface AssetConsumption {

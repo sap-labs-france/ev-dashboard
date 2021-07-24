1c394ee2fb826b17a4a8a3105618c9e14830bf9b
LucasBrazi06 <sergefabiano@free.fr>
2021-07-24 17:20:21 +0200

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -33,6 +33,7 @@ export class ReleaseNotesComponent {
               `Fixed Site Admin cannot see Statistics of his Users`,
               `Fixed missing IDs in RESTful file documentation`,
               `Lacroix / Linky Asset integration`,
+              `Check Charging Station Registration Token at Web Socket connection and refuse it if not valid`,
               `Set OCPI Opening Hours and Tariff ID for SLF Caen`,
             ],
           },

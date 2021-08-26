9da15496a011fe7a1e62911b2f551ea67aa83e7a
LucasBrazi06 <sergefabiano@free.fr>
2021-08-26 10:23:23 +0200

    Updated version

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -23,6 +23,18 @@ export class ReleaseNotesComponent {
   public buildReleaseNotes() {
     this.releaseNotes = [
       {
+        version: '2.4.88',
+        date: new Date('2021-08-25'),
+        componentChanges: [
+          {
+            name: 'Dashboard',
+            changes: [
+              `Renamed 'ids' to 'IDs' in JSON Schemas`,
+            ],
+          },
+        ],
+      },
+      {
         version: '2.4.87',
         date: new Date('2021-08-25'),
         componentChanges: [

f96788e44f6b17dff95070794e90c8d94e06ec54
rmangin-fr <remi.mangin@sap.com>
2021-09-30 16:00:22 +0200

    change filter type

--- src/app/pages/users/filters/user-technical-filter.ts
+++ src/app/pages/users/filters/user-technical-filter.ts
@@ -1,20 +1,30 @@
-import { CentralServerService } from '../../../services/central-server.service';
 import { TableFilter } from '../../../shared/table/filters/table-filter';
+import { KeyValue } from '../../../types/GlobalType';
 import { FilterType, TableFilterDef } from '../../../types/Table';
 
 export class UserTechnicalFilter extends TableFilter {
-  public constructor() {
+  public constructor(defaultValue = false) {
     super();
     // Define filter
     const filterDef: TableFilterDef = {
       id: 'technical',
       httpId: 'Technical',
-      type: FilterType.SWITCH,
+      type: FilterType.DROPDOWN,
+      multiple: true,
+      exhaustive: true,
       name: 'users.technical',
-      class: 'col-md-6 col-lg-4 col-xl-2',
+      class: 'col-md-6 col-lg-3 col-xl-2',
       label: 'users.technical',
+      cleared: true,
       currentValue: true,
+      items: Object.assign([], technicalValues),
+      defaultValue: defaultValue ? technicalValues[1] : technicalValues[0],
     };
     this.setFilterDef(filterDef);
   }
 }
+
+export const technicalValues: KeyValue[] = [
+  { key: 'false', value: 'general.no' },
+  { key: 'true', value: 'general.yes' },
+];

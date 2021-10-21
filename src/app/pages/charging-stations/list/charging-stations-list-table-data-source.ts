6587c51e5b99fc3bef2a7fa7fa46bb65812f9e89
LucasBrazi06 <sergefabiano@free.fr>
2021-10-21 10:50:00 +0200

    Remove socketIO due to scaling issues

--- src/app/pages/charging-stations/list/charging-stations-list-table-data-source.ts
+++ src/app/pages/charging-stations/list/charging-stations-list-table-data-source.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs';
 import { CompanyTableFilter } from 'shared/table/filters/company-table-filter';
 
 import { AuthorizationService } from '../../../services/authorization.service';
-import { CentralServerNotificationService } from '../../../services/central-server-notification.service';
 import { CentralServerService } from '../../../services/central-server.service';
 import { ComponentService } from '../../../services/component.service';
 import { DialogService } from '../../../services/dialog.service';
@@ -30,7 +29,6 @@ import { IssuerFilter } from '../../../shared/table/filters/issuer-filter';
 import { SiteAreaTableFilter } from '../../../shared/table/filters/site-area-table-filter';
 import { SiteTableFilter } from '../../../shared/table/filters/site-table-filter';
 import { TableDataSource } from '../../../shared/table/table-data-source';
-import ChangeNotification from '../../../types/ChangeNotification';
 import { ChargePointStatus, ChargingStation, ChargingStationButtonAction, Connector, FirmwareStatus } from '../../../types/ChargingStation';
 import { DataResult } from '../../../types/DataResult';
 import { ButtonAction } from '../../../types/GlobalType';
@@ -58,7 +56,6 @@ export class ChargingStationsListTableDataSource extends TableDataSource<Chargin
     public translateService: TranslateService,
     private messageService: MessageService,
     private router: Router,
-    private centralServerNotificationService: CentralServerNotificationService,
     private centralServerService: CentralServerService,
     private authorizationService: AuthorizationService,
     private componentService: ComponentService,
@@ -75,10 +72,6 @@ export class ChargingStationsListTableDataSource extends TableDataSource<Chargin
       }]);
     }
     this.initDataSource();
-  }
-
-  public getDataChangeSubject(): Observable<ChangeNotification> {
-    return this.centralServerNotificationService.getSubjectChargingStations();
   }
 
   public loadDataImpl(): Observable<DataResult<ChargingStation>> {

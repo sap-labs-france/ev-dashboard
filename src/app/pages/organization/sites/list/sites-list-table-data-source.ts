7a6b8f6274d8d7f0cd52b40d4005c377d691002d
BANCHEREAU Caroline <caroline.banchereau@sap.com>
2021-11-19 15:57:04 +0100

    modified pricing settings i18n reference in settings integration added pricing to sites

--- src/app/pages/organization/sites/list/sites-list-table-data-source.ts
+++ src/app/pages/organization/sites/list/sites-list-table-data-source.ts
@@ -4,7 +4,10 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { AuthorizationService } from 'services/authorization.service';
+import { PricingDefinitionsDialogComponent } from 'shared/pricing-definitions/pricing-definitions.dialog.component';
 import { TableSiteGenerateQrCodeConnectorAction, TableSiteGenerateQrCodeConnectorsActionDef } from 'shared/table/actions/sites/table-site-generate-qr-code-connector-action';
+import { TableViewPricingsAction, TableViewPricingsActionDef } from 'shared/table/actions/table-view-pricings-action';
+import { PricingButton, PricingEntity } from 'types/Pricing';
 
 import { CentralServerService } from '../../../../services/central-server.service';
 import { DialogService } from '../../../../services/dialog.service';
@@ -45,7 +48,7 @@ export class SitesListTableDataSource extends TableDataSource<Site> {
   private exportOCPPParamsAction = new TableExportOCPPParamsAction().getActionDef();
   private siteGenerateQrCodeConnectorAction = new TableSiteGenerateQrCodeConnectorAction().getActionDef();
   private createAction = new TableCreateSiteAction().getActionDef();
-
+  private viewPricingsAction = new TableViewPricingsAction().getActionDef();
 
   public constructor(
     public spinnerService: SpinnerService,
@@ -212,6 +215,7 @@ export class SitesListTableDataSource extends TableDataSource<Site> {
       moreActions.addActionInMoreActions(this.deleteAction);
     }
     moreActions.addActionInMoreActions(openInMaps);
+    moreActions.addActionInMoreActions(this.viewPricingsAction);
     rowActions.push(moreActions.getActionDef());
     return rowActions;
   }
@@ -279,6 +283,19 @@ export class SitesListTableDataSource extends TableDataSource<Site> {
             site, this.translateService, this.spinnerService,
             this.messageService, this.centralServerService, this.router
           );
+        }
+        break;
+      case PricingButton.VIEW_PRICING_LIST:
+        if (actionDef.action) {
+          (actionDef as TableViewPricingsActionDef).action(PricingDefinitionsDialogComponent, this.dialog, {
+            dialogData: {
+              id: null,
+              context: {
+                entityID: site.id,
+                entityType: PricingEntity.SITE
+              }
+            },
+          }, this.refreshData.bind(this));
         }
         break;
     }

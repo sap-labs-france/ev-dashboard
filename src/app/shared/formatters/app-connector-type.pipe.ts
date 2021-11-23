b33bb4f16025ea662a34116200e1e93eadeb6d2d
ClaudeROSSI <claude.rossi@sap.com>
2021-11-23 12:06:22 +0100

    pricing - minor changes

--- src/app/shared/formatters/app-connector-type.pipe.ts
+++ src/app/shared/formatters/app-connector-type.pipe.ts
@@ -2,52 +2,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { CONNECTOR_ALL_TYPES_MAP } from '../../types/Connector';
 
-// export const CONNECTOR_TYPE_MAP =
-//   [
-//     {
-//       key: 'T2',
-//       description: 'chargers.connector_type_type2',
-//       svgIconName: 'T2',
-//       svgIconFile: 'assets/img/connectors/type2.svg',
-//     },
-//     {
-//       key: 'CCS',
-//       description: 'chargers.connector_type_combo',
-//       svgIconName: 'CCS',
-//       svgIconFile: 'assets/img/connectors/combo-ccs.svg',
-//     },
-//     {
-//       key: 'C',
-//       description: 'chargers.connector_type_chademo',
-//       svgIconName: 'C',
-//       svgIconFile: 'assets/img/connectors/chademo.svg',
-//     },
-//     {
-//       key: 'T1',
-//       description: 'chargers.connector_type_type1',
-//       svgIconName: 'T1',
-//       svgIconFile: 'assets/img/connectors/type1.svg',
-//     },
-//     {
-//       key: 'T1CCS',
-//       description: 'chargers.connector_type_type1ccs',
-//       svgIconName: 'T1CCS',
-//       svgIconFile: 'assets/img/connectors/type1-ccs.svg',
-//     },
-//     {
-//       key: 'D',
-//       description: 'chargers.connector_type_domestic',
-//       svgIconName: 'D',
-//       svgIconFile: 'assets/img/connectors/domestic-ue.svg',
-//     },
-//     {
-//       key: 'U',
-//       description: 'chargers.connector_type_unknown',
-//       svgIconName: 'U',
-//       svgIconFile: 'assets/img/connectors/no-connector.svg',
-//     },
-//   ];
-
 export const CONNECTOR_TYPE_MAP = [...CONNECTOR_ALL_TYPES_MAP.values()].filter((type: any) => type.key !== 'A');;
 
 @Pipe({ name: 'appConnectorType' })

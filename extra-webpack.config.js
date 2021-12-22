82e49dac2c6640bd514153068c4224f479ba90f2
ClaudeROSSI <claude.rossi@sap.com>
2021-12-22 15:52:41 +0100

    locale - webpack config adaptation

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -4,10 +4,8 @@ MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin');
 module.exports = {
   plugins: [
     new MomentLocalesPlugin({
-      // This has no impact!
-      // TODO - how to consider en_AU specific use-case?
-      // TODO - cs_CZ is missing!
-      localesToKeep: ['en', 'fr', 'es', 'de', 'pt', 'it']
+      // This seems to have no impact!
+      localesToKeep: ['en', 'en-us', 'en-gb', 'en-au', 'fr', 'es', 'de', 'pt', 'it', 'cs']
     }),
     new MomentTimezoneDataPlugin({
       startYear: new Date().getFullYear() - 2,

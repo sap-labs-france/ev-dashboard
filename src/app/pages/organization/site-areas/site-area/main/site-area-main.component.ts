aa50804b6626d6d28ebe26122e08104fa23d9118
LucasBrazi06 <sergefabiano@free.fr>
2022-01-18 16:31:19 +0100

    Clean up Alignment with Site component

--- src/app/pages/organization/site-areas/site-area/main/site-area-main.component.ts
+++ src/app/pages/organization/site-areas/site-area/main/site-area-main.component.ts
@@ -1,24 +1,25 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 import * as moment from 'moment';
 
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { Address } from '../../../../../types/Address';
+import { ButtonType } from '../../../../../types/Table';
 import { CentralServerService } from '../../../../../services/central-server.service';
 import { ComponentService } from '../../../../../services/component.service';
 import { ConfigService } from '../../../../../services/config.service';
+import { Constants } from '../../../../../utils/Constants';
 import { DialogService } from '../../../../../services/dialog.service';
 import { MessageService } from '../../../../../services/message.service';
-import { SpinnerService } from '../../../../../services/spinner.service';
-import { SitesDialogComponent } from '../../../../../shared/dialogs/sites/sites-dialog.component';
-import { Address } from '../../../../../types/Address';
 import { RegistrationToken } from '../../../../../types/RegistrationToken';
+import { Router } from '@angular/router';
 import { Site } from '../../../../../types/Site';
 import { SiteArea } from '../../../../../types/SiteArea';
-import { ButtonType } from '../../../../../types/Table';
+import { SitesDialogComponent } from '../../../../../shared/dialogs/sites/sites-dialog.component';
+import { SpinnerService } from '../../../../../services/spinner.service';
 import { TenantComponents } from '../../../../../types/Tenant';
-import { Constants } from '../../../../../utils/Constants';
+import { TranslateService } from '@ngx-translate/core';
 import { Utils } from '../../../../../utils/Utils';
 
 @Component({
@@ -33,7 +34,7 @@ export class SiteAreaMainComponent implements OnInit,OnChanges {
   @Output() public publicChanged = new EventEmitter<boolean>();
 
   public image = Constants.NO_IMAGE;
-  public imageHasChanged = false;
+  public siteAreaImageSet = false;
   public maxSize: number;
 
   public issuer!: AbstractControl;
@@ -54,8 +55,6 @@ export class SiteAreaMainComponent implements OnInit,OnChanges {
   ];
 
   public address!: Address;
-  public canCreateSiteArea = false;
-  public canUpdateSiteArea = false;
   public isSmartChargingComponentActive = false;
 
   public registrationToken!: RegistrationToken;
@@ -129,6 +128,9 @@ export class SiteAreaMainComponent implements OnInit,OnChanges {
     this.voltage = this.formGroup.controls['voltage'];
     this.numberOfPhases = this.formGroup.controls['numberOfPhases'];
     this.maximumPowerAmps.disable();
+    if (this.readOnly) {
+      this.formGroup.disable();
+    }
     if (this.currentSiteAreaID) {
       this.loadRegistrationToken();
     }
@@ -139,51 +141,53 @@ export class SiteAreaMainComponent implements OnInit,OnChanges {
   }
 
   public loadSiteArea() {
-    if (!this.siteArea) {
-      return;
-    }
-    if (Utils.objectHasProperty(this.siteArea, 'issuer')) {
-      this.formGroup.controls.issuer.setValue(this.siteArea.issuer);
-    }
-    if (this.siteArea.id) {
+    if (this.siteArea) {
       this.formGroup.controls.id.setValue(this.siteArea.id);
+      if (Utils.objectHasProperty(this.siteArea, 'issuer')) {
+        this.formGroup.controls.issuer.setValue(this.siteArea.issuer);
+      }
+      if (this.siteArea.name) {
+        this.formGroup.controls.name.setValue(this.siteArea.name);
+      }
+      if (this.siteArea.siteID) {
+        this.formGroup.controls.siteID.setValue(this.siteArea.siteID);
+      }
+      if (this.siteArea.site) {
+        this.site.setValue(this.siteArea.site.name);
+      }
+      if (this.siteArea.maximumPower) {
+        this.formGroup.controls.maximumPower.setValue(this.siteArea.maximumPower);
+      }
+      if (this.siteArea.numberOfPhases) {
+        this.formGroup.controls.numberOfPhases.setValue(this.siteArea.numberOfPhases);
+      }
+      if (this.siteArea.voltage) {
+        this.formGroup.controls.voltage.setValue(this.siteArea.voltage);
+      }
+      if (this.siteArea.smartCharging) {
+        this.formGroup.controls.smartCharging.setValue(this.siteArea.smartCharging);
+      } else {
+        this.formGroup.controls.smartCharging.setValue(false);
+      }
+      if (this.siteArea.accessControl) {
+        this.formGroup.controls.accessControl.setValue(this.siteArea.accessControl);
+      } else {
+        this.formGroup.controls.accessControl.setValue(false);
+      }
+      if (this.siteArea.address) {
+        this.address = this.siteArea.address;
+      }
+      this.refreshMaximumAmps();
+      // Get Site Area image
+      if (!this.siteAreaImageSet) {
+        this.centralServerService.getSiteAreaImage(this.currentSiteAreaID).subscribe((siteAreaImage) => {
+          this.siteAreaImageSet = true;
+          if (siteAreaImage) {
+            this.image = siteAreaImage;
+          }
+        });
+      }
     }
-    if (this.siteArea.name) {
-      this.formGroup.controls.name.setValue(this.siteArea.name);
-    }
-    if (this.siteArea.siteID) {
-      this.formGroup.controls.siteID.setValue(this.siteArea.siteID);
-    }
-    if (this.siteArea.site) {
-      this.site.setValue(this.siteArea.site.name);
-    }
-    if (this.siteArea.maximumPower) {
-      this.formGroup.controls.maximumPower.setValue(this.siteArea.maximumPower);
-    }
-    if (this.siteArea.numberOfPhases) {
-      this.formGroup.controls.numberOfPhases.setValue(this.siteArea.numberOfPhases);
-    }
-    if (this.siteArea.voltage) {
-      this.formGroup.controls.voltage.setValue(this.siteArea.voltage);
-    }
-    if (this.siteArea.smartCharging) {
-      this.formGroup.controls.smartCharging.setValue(this.siteArea.smartCharging);
-    } else {
-      this.formGroup.controls.smartCharging.setValue(false);
-    }
-    if (this.siteArea.accessControl) {
-      this.formGroup.controls.accessControl.setValue(this.siteArea.accessControl);
-    } else {
-      this.formGroup.controls.accessControl.setValue(false);
-    }
-    if (this.siteArea.address) {
-      this.address = this.siteArea.address;
-    }
-    this.refreshMaximumAmps();
-    // Get Site image
-    this.centralServerService.getSiteAreaImage(this.currentSiteAreaID).subscribe((siteAreaImage) => {
-      this.image = siteAreaImage ? siteAreaImage : Constants.NO_IMAGE;
-    });
   }
 
   public assignSite() {
@@ -212,28 +216,58 @@ export class SiteAreaMainComponent implements OnInit,OnChanges {
     });
   }
 
+  public updateSiteAreaCoordinates(siteArea: SiteArea) {
+    if (siteArea.address && siteArea.address.coordinates &&
+      !(siteArea.address.coordinates[0] || siteArea.address.coordinates[1])) {
+      delete siteArea.address.coordinates;
+    }
+  }
+
   public voltageChanged() {
     this.maximumPowerChanged();
   }
 
   public updateSiteAreaImage(siteArea: SiteArea) {
-    if (this.imageHasChanged) {
-      // Set new image
-      if (this.image !== Constants.NO_IMAGE) {
-        siteArea.image = this.image;
-      } else {
-        siteArea.image = null;
-      }
+    if (this.image !== Constants.USER_NO_PICTURE) {
+      siteArea.image = this.image;
     } else {
-      // No changes
-      delete siteArea.image;
+      siteArea.image = null;
     }
   }
 
-  public updateSiteAreaCoordinates(siteArea: SiteArea) {
-    if (siteArea.address && siteArea.address.coordinates &&
-      !(siteArea.address.coordinates[0] || siteArea.address.coordinates[1])) {
-      delete siteArea.address.coordinates;
+  public clearImage() {
+    this.image = Constants.NO_IMAGE;
+    this.siteAreaImageSet = false;
+    this.formGroup.markAsDirty();
+  }
+
+  public onImageChanged(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      if (file.size > (this.maxSize * 1024)) {
+        this.messageService.showErrorMessage('site_areas.image_size_error', { maxPictureKb: this.maxSize });
+      } else {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.image = reader.result as string;
+          this.siteAreaImageSet = true;
+          this.formGroup.markAsDirty();
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  public refreshMaximumAmps() {
+    this.maximumPowerChanged();
+  }
+
+  public maximumPowerChanged() {
+    if (!this.maximumPower.errors && this.voltage.value) {
+      this.maximumPowerAmps.setValue(
+        Math.floor((this.maximumPower.value as number) / (this.voltage.value as number)));
+    } else {
+      this.maximumPowerAmps.setValue(0);
     }
   }
 
@@ -273,61 +307,20 @@ export class SiteAreaMainComponent implements OnInit,OnChanges {
     this.messageService.showInfoMessage('chargers.connections.url_copied');
   }
 
-  public onImageChanged(event: any) {
-    // load picture
-    if (event.target.files && event.target.files[0]) {
-      const file = event.target.files[0];
-      if (file.size > (this.maxSize * 1024)) {
-        this.messageService.showErrorMessage('site_areas.image_size_error', { maxPictureKb: this.maxSize });
-      } else {
-        const reader = new FileReader();
-        reader.onload = () => {
-          this.image = reader.result as string;
-          this.imageHasChanged = true;
-          this.formGroup.markAsDirty();
-        };
-        reader.readAsDataURL(file);
-      }
-    }
-  }
-
-  public clearImage() {
-    // Clear
-    this.image = Constants.NO_IMAGE;
-    this.imageHasChanged = true;
-    // Set form dirty
-    this.formGroup.markAsDirty();
-  }
-
-  public refreshMaximumAmps() {
-    this.maximumPowerChanged();
-  }
-
-  public maximumPowerChanged() {
-    if (!this.maximumPower.errors && this.voltage.value) {
-      this.maximumPowerAmps.setValue(
-        Math.floor((this.maximumPower.value as number) / (this.voltage.value as number)));
-    } else {
-      this.maximumPowerAmps.setValue(0);
-    }
-  }
-
   private loadRegistrationToken() {
-    if (!this.currentSiteAreaID) {
-      return;
-    }
-    this.centralServerService.getRegistrationTokens({
-      SiteAreaID: this.currentSiteAreaID,
-    }).subscribe(((dataResult) => {
-      if (dataResult && dataResult.result) {
-        for (const registrationToken of dataResult.result) {
-          if (this.isRegistrationTokenValid(registrationToken)) {
-            this.registrationToken = registrationToken;
-            break;
+    if (this.currentSiteAreaID) {
+      this.centralServerService.getRegistrationTokens(
+        { SiteAreaID: this.currentSiteAreaID }).subscribe(((dataResult) => {
+        if (dataResult && dataResult.result) {
+          for (const registrationToken of dataResult.result) {
+            if (this.isRegistrationTokenValid(registrationToken)) {
+              this.registrationToken = registrationToken;
+              break;
+            }
           }
         }
-      }
-    }));
+      }));
+    }
   }
 
   private isRegistrationTokenValid(registrationToken: RegistrationToken): boolean {

dd4d1fbc549e6f9fcae84be5a26032c99b1e5fde
LucasBrazi06 <sergefabiano@free.fr>
2022-01-18 14:37:27 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -24,7 +24,7 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.5.15',
-        date: new Date('2022-01-16'),
+        date: new Date('2022-01-18'),
         componentChanges: [
           {
             name: 'Dashboard',

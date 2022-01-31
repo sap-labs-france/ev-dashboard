b9bc9a3421c639cbe9014936e17b2f94ee2cda55
LucasBrazi06 <sergefabiano@free.fr>
2022-01-31 15:41:20 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -24,14 +24,15 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.5.16',
-        date: new Date('2022-01-28'),
+        date: new Date('2022-01-31'),
         componentChanges: [
           {
             name: 'Dashboard',
             changes: [
               `Final Pricing engine`,
-              `Refactor aggregation of Sites' consumption charts`,
-              `Fixed size of all pop-ups`,
+              `Fixed synchromization of new Cars`,
+              `Refactor aggregation of consumption charts for Sites`,
+              `Fixed pop-up wrong size issues`,
               `Cleanup of unused Billing jobs`,
             ],
           },

3ae9bece56a32cf5ff8ebd55445d31199080879c
LucasBrazi06 <sergefabiano@free.fr>
2022-02-14 09:48:51 +0100

    Fixed typo

--- src/app/shared/table/filters/date-range-table-filter.ts
+++ src/app/shared/table/filters/date-range-table-filter.ts
@@ -6,7 +6,6 @@ import { FilterType, TableFilterDef } from '../../../types/Table';
 import { TableFilter } from './table-filter';
 
 export class DateRangeTableFilter extends TableFilter {
-
   private allRanges: any = {};
   private translateService: TranslateService;
 

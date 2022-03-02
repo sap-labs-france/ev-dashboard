b6252cc65d5f23fdd42f24915123ff3ce9477ecf
LucasBrazi06 <sergefabiano@free.fr>
2022-03-02 15:41:45 +0100

    Updated release notes

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -23,6 +23,19 @@ export class ReleaseNotesComponent {
   public buildReleaseNotes() {
     this.releaseNotes = [
       {
+        version: '2.6.3',
+        date: new Date('2022-03-02'),
+        componentChanges: [
+          {
+            name: 'Dashboard',
+            changes: [
+              `Enhanced Web Socket management in Charging Station's server`,
+              `Fixed OCPI must return Charging when OCPP status is SuspendedEV/EVSE`,
+            ],
+          },
+        ],
+      },
+      {
         version: '2.6.2',
         date: new Date('2022-02-26'),
         componentChanges: [

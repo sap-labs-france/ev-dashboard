3d95cc369c7b7660affd7affaa04ece2abbe5912
LucasBrazi06 <sergefabiano@free.fr>
2022-04-14 12:03:40 +0200

    Fixed CS Registration CSS issue

--- src/app/release-notes/release-notes.component.ts
+++ src/app/release-notes/release-notes.component.ts
@@ -25,7 +25,7 @@ export class ReleaseNotesComponent {
     this.releaseNotes = [
       {
         version: '2.6.5',
-        date: new Date('2022-04-13'),
+        date: new Date('2022-04-14'),
         componentChanges: [
           {
             name: 'Dashboard',

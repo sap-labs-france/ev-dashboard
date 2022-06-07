816cd55727e20d43588a6cafc3d515f4fbd1fa84
mahdibenromdhane <mahdi.benromdhane@esprit.tn>
2022-06-07 15:49:07 +0200

    update cli and core to 12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -6,7 +6,7 @@ import '@angular/localize/init';
  * Zone JS is required by default for Angular itself.
  */
 // Included with Angular CLI.
-import 'zone.js/dist/zone';
+import 'zone.js';
 
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.

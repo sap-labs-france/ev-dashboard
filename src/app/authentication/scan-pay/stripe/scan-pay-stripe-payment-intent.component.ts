23e8327a8fd00e84dcf04d0fc1f97b519ad52baf
BANCHEREAU Caroline <caroline.banchereau@sap.com>
2023-03-13 10:08:15 +0100

    error messages clear for payment intent succeeded / failed

--- src/app/authentication/scan-pay/stripe/scan-pay-stripe-payment-intent.component.ts
+++ src/app/authentication/scan-pay/stripe/scan-pay-stripe-payment-intent.component.ts
@@ -33,6 +33,7 @@ export class ScanPayStripePaymentIntentComponent implements OnInit {
   public token: string;
   public isSendClicked: boolean;
   public isTokenValid = true;
+  public isBackendConnectionValid = false;
   // Stripe elements
   public elements: StripeElements;
   public paymentElement: StripePaymentElement;
@@ -49,6 +50,7 @@ export class ScanPayStripePaymentIntentComponent implements OnInit {
     public windowService: WindowService,
     public authorizationService: AuthorizationService) {
     this.isBillingComponentActive = true; // comment on gere ça ??
+    // this.isBackendConnectionValid = false;
   }
 
   public ngOnInit(): void {
@@ -120,15 +122,16 @@ export class ScanPayStripePaymentIntentComponent implements OnInit {
         elements: this.elements,
         redirect: 'if_required'
       });
-      if (operationResult.error) {
+      if (operationResult?.error) {
         // 3DS authentication has been aborted or user was not able to authenticate
         this.messageService.showErrorMessage('settings.billing.payment_intent_create_error', { stripeError: operationResult.error.message });
       } else {
         // Operation succeeded - try to start transaction
         const operationResultRetrieve: any = await this.retrievePaymentIntentAndStartTransaction();
-        if (operationResultRetrieve.error) {
+        if (operationResultRetrieve?.error) {
           this.messageService.showErrorMessage('settings.billing.payment_intent_create_error', { stripeError: operationResult.error.message });
         } else {
+          this.isBackendConnectionValid = true;
           this.messageService.showSuccessMessage('settings.billing.payment_intent_create_success');
         }
         this.isSendClicked = true;
